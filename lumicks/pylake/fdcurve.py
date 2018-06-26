577f5be75b5b4abd3f9e39637651706d6e8fa393
Dean Moldovan <d.moldovan@lumicks.com>
2018-06-26 18:08:58 +0200

    Avoid storing a HDF5 dataset inside FDCurves

--- lumicks/pylake/fdcurve.py
+++ lumicks/pylake/fdcurve.py
@@ -1,3 +1,4 @@
+from copy import copy
 from .detail.mixin import DownsampledFD
 
 
@@ -8,21 +9,39 @@ class FDCurve(DownsampledFD):
     and `distance1`. Alternatives can be selected using `FDCurve.with_channels()`.
     Note that it does not modify the FD curve in place but returns a copy.
 
-    Parameters
+    Attributes
     ----------
-    h5py_dset : h5py.Dataset
-        The original HDF5 dataset containing FD curve information
     file : lumicks.pylake.File
-        The parent file. Used to loop up channel data.
+        The parent file. Used to look up channel data.
+    start, stop : int
+        The time range (ns) of this FD curve within the file.
+    name : str
+        The name of this FD curve as it appeared in the timeline.
     """
-    def __init__(self, h5py_dset, file, force="2", distance="1"):
-        self.dset = h5py_dset
-        self.start = h5py_dset.attrs["Start time (ns)"]
-        self.stop = h5py_dset.attrs["Stop time (ns)"]
-        self.name = h5py_dset.name.split("/")[-1]
+    def __init__(self, file, start, stop, name, force="2", distance="1"):
         self.file = file
+        self.start = start
+        self.stop = stop
+        self.name = name
         self._primary_force_channel = force
         self._primary_distance_channel = distance
+        self._force_cache = None
+        self._distance_cache = None
+
+    @classmethod
+    def from_dset(cls, h5py_dset, file, **kwargs):
+        return cls(file=file, start=h5py_dset.attrs["Start time (ns)"],
+                   stop=h5py_dset.attrs["Stop time (ns)"], name=h5py_dset.name.split("/")[-1],
+                   **kwargs)
+
+    def __copy__(self):
+        """Custom implementation of copy because the caches need to be cleared"""
+        cls = self.__class__
+        new_copy = cls.__new__(cls)
+        new_copy.__dict__.update(self.__dict__)
+        new_copy._force_cache = None
+        new_copy._distance_cache = None
+        return new_copy
 
     def _get_downsampled_force(self, n, xy):
         return getattr(self.file, f"downsampled_force{n}{xy}")[self.start:self.stop]
@@ -33,16 +52,23 @@ class FDCurve(DownsampledFD):
     @property
     def f(self):
         """The primary force channel associated with this FD curve"""
-        return getattr(self, f"downsampled_force{self._primary_force_channel}")
+        if self._force_cache is None:
+            self._force_cache = getattr(self, f"downsampled_force{self._primary_force_channel}")
+        return self._force_cache
 
     @property
     def d(self):
         """The primary distance channel associated with this FD curve"""
-        return getattr(self, f"distance{self._primary_distance_channel}")
+        if self._distance_cache is None:
+            self._distance_cache = getattr(self, f"distance{self._primary_distance_channel}")
+        return self._distance_cache
 
     def with_channels(self, force, distance):
         """Return a copy of this FD curve with difference primary force and distance channels"""
-        return FDCurve(self.dset, self.file, force, distance)
+        new_fd = copy(self)
+        new_fd._primary_force_channel = force
+        new_fd._primary_distance_channel = distance
+        return new_fd
 
     def plot_scatter(self, **kwargs):
         """Plot the FD curve points
@@ -57,3 +83,4 @@ class FDCurve(DownsampledFD):
         plt.scatter(self.d.data, self.f.data, **{"s": 8, **kwargs})
         plt.xlabel(self.d.labels.get("y", "distance"))
         plt.ylabel(self.f.labels.get("y", "force"))
+        plt.title(self.name)
