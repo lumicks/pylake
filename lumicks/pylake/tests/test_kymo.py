3e84dbd885444271bd7f53b399e64a5845c790bd
JoepVanlier <j.vanlier@lumicks.com>
2019-09-03 09:26:29 +0200

    Also test the content post-slicing, not just the shape.

--- lumicks/pylake/tests/test_kymo.py
+++ lumicks/pylake/tests/test_kymo.py
@@ -7,16 +7,47 @@ def test_kymo_slicing(h5_file):
     f = pylake.File.from_h5py(h5_file)
     if f.format_version == 2:
         kymo = f.kymos["Kymo1"]
+
+        kymo_reference = np.transpose([[2, 0, 0, 0, 2], [0, 0, 0, 0, 0], [1, 0, 0, 0, 1], [0, 1, 1, 1, 0]])
+
         assert kymo.red_image.shape == (5, 4)
-        assert kymo[:].red_image.shape == (5, 4)
-        assert kymo["1s":].red_image.shape == (5, 3)
-        assert kymo["0s":].red_image.shape == (5, 4)
-        assert kymo["0s":"2s"].red_image.shape == (5, 2)
-        assert kymo["0s":"-1s"].red_image.shape == (5, 3)
-        assert kymo["0s":"-2s"].red_image.shape == (5, 2)
-        assert kymo["0s":"3s"].red_image.shape == (5, 3)
-        assert kymo["1s":"2s"].red_image.shape == (5, 1)
-        assert kymo["0s":"10s"].red_image.shape == (5, 4)
+        assert np.allclose(kymo.red_image.data, kymo_reference)
+
+        sliced = kymo[:]
+        assert sliced.red_image.shape == (5, 4)
+        assert np.allclose(sliced.red_image.data, kymo_reference)
+
+        sliced = kymo["1s":]
+        assert sliced.red_image.shape == (5, 3)
+        assert np.allclose(sliced.red_image.data, kymo_reference[:, 1:])
+
+        sliced = kymo["0s":]
+        assert sliced.red_image.shape == (5, 4)
+        assert np.allclose(sliced.red_image.data, kymo_reference)
+
+        sliced = kymo["0s":"2s"]
+        assert sliced.red_image.shape == (5, 2)
+        assert np.allclose(sliced.red_image.data, kymo_reference[:, :2])
+
+        sliced = kymo["0s":"-1s"]
+        assert sliced.red_image.shape == (5, 3)
+        assert np.allclose(sliced.red_image.data, kymo_reference[:, :-1])
+
+        sliced = kymo["0s":"-2s"]
+        assert sliced.red_image.shape == (5, 2)
+        assert np.allclose(sliced.red_image.data, kymo_reference[:, :-2])
+
+        sliced = kymo["0s":"3s"]
+        assert sliced.red_image.shape == (5, 3)
+        assert np.allclose(sliced.red_image.data, kymo_reference[:, :3])
+
+        sliced = kymo["1s":"2s"]
+        assert sliced.red_image.shape == (5, 1)
+        assert np.allclose(sliced.red_image.data, kymo_reference[:, 1:2])
+
+        sliced = kymo["0s":"10s"]
+        assert sliced.red_image.shape == (5, 4)
+        assert np.allclose(sliced.red_image.data, kymo_reference[:, 0:10])
 
         with pytest.raises(IndexError):
             kymo["5s":]
