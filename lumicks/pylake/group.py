2aa2facbe9726d380383a5cb2828747194b4d711
JoepVanlier <j.vanlier@lumicks.com>
2019-09-10 13:22:56 +0200

    Added keys(), iterator and repr for group

--- lumicks/pylake/group.py
+++ lumicks/pylake/group.py
@@ -12,6 +12,7 @@ class Group:
     """
     def __init__(self, h5py_group):
         self.h5 = h5py_group
+        self._idx = -1
 
     def __getitem__(self, item):
         """Return a subgroup or a bluelake timeline channel"""
@@ -21,3 +22,28 @@ class Group:
         else:
             cls = channel_class(thing)
             return cls.from_dataset(thing)
+
+    def keys(self):
+        """Return group names at this level"""
+        return list(self.h5.keys())
+
+    def __iter__(self):
+        self._idx = -1
+        return self
+
+    def __next__(self):
+        """Return key names for the iterator analogously to HDF5"""
+        if self._idx >= len(self.keys()) - 1:
+            raise StopIteration
+
+        self._idx += 1
+        return self.keys()[self._idx]
+
+    def __repr__(self):
+        """Return type name and members of the group"""
+        name = self.__class__.__name__
+        members = ''.join(f"{e}, " for e in self.keys())
+        if members:
+            return f"{name} (members: {members[:-2]})"
+        else:
+            return f"{name}"
