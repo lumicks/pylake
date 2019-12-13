5b61dbe78da53b32aee07a3d33652777378bc8c4
Joep Vanlier <joep.vanlier@gmail.com>
2019-12-13 11:35:02 +0100

    Added derivative for invWLC model. Modified defaults for invWLC model. Improved numerical stability for invWLC jacobian.

--- lumicks/pylake/fdmodels.py
+++ lumicks/pylake/fdmodels.py
@@ -29,7 +29,7 @@ def force_model(name, model_type):
     kT_default = Parameter(value=4.11, lb=0.0, ub=8.0, vary=False, shared=True)
     Lp_default = Parameter(value=40.0, lb=0.0, ub=np.inf)
     Lc_default = Parameter(value=16.0, lb=0.0, ub=np.inf)
-    St_default = Parameter(value=750.0, lb=0.0, ub=np.inf)
+    St_default = Parameter(value=1500.0, lb=0.0, ub=np.inf)
     if model_type == "offset":
         return Model(name, offset_model, offset_model_jac, derivative=offset_model_derivative,
                      offset=Parameter(value=0, lb=-np.inf, ub=np.inf))
@@ -48,21 +48,21 @@ def force_model(name, model_type):
         return Model(name, FJC, FJC_jac, derivative=FJC_derivative,
                      kT=kT_default, Lp=Lp_default, Lc=Lc_default, St=St_default)
     elif model_type == "invWLC":
-        return Model(name, invWLC, invWLC_jac, kT=kT_default, Lp=Lp_default, Lc=Lc_default, St=St_default)
-        #return InverseModel(force_model("WLC"))
+        return Model(name, invWLC, invWLC_jac, derivative=invWLC_derivative,
+                     kT=kT_default, Lp=Lp_default, Lc=Lc_default, St=St_default)
     elif model_type == "invtWLC":
-        return Model(name, invtWLC, invtWLC_jac, derivative=tWLC_derivative,
+        return Model(name, invtWLC, invtWLC_jac,
                      kT=kT_default, Lp=Lp_default, Lc=Lc_default, St=St_default,
                      Fc=Parameter(value=30.6, lb=0.0, ub=50000.0),
                      C=Parameter(value=440.0, lb=0.0, ub=50000.0),
                      g0=Parameter(value=-637, lb=-50000.0, ub=50000.0),
                      g1=Parameter(value=17.0, lb=-50000.0, ub=50000.0),
                      )
-        #return InverseModel(force_model("tWLC"))
     elif model_type == "invFJC":
         return InverseModel(force_model(name, "FJC"))
     else:
-        raise ValueError("Invalid model selected. Valid options are WLC, tWLC, FJC, invWLC, invtWLC, invFJC.")
+        raise ValueError(f"Invalid model {model_type} selected. Valid options are WLC, tWLC, FJC, invWLC, invtWLC, "
+                         f"invFJC.")
 
 
 def offset_model(x, offset):
@@ -279,137 +279,188 @@ def invWLC(d, Lp, Lc, St, kT = 4.11):
             (1.0/3.0) * x2 * (Lc * d * x0 - x0 * x1)
 
 
-def invWLC_jac(d, Lp, Lc, St, kT = 4.11):
+def invWLC_jac(d, Lp, Lc, St, kT=4.11):
     x0 = Lc * St
     x1 = 2.0 * d
     x2 = x0 * x1
-    x3 = Lc * Lc
-    x49 = Lc * x3  # Lc^3
-    x44 = x3 * x3  # Lc^4
-    x52 = x3 * x49   # Lc^5
-    x41 = x49 * x49  # Lc^6
-    x31 = x41 * x49  # Lc^9
-    x33 = Lc * x31   # Lc^10
-    x20 = Lc * x33   # Lc^11
-    x15 = x41 * x41  # Lc^12
-    x4 = 2.0 * x3
+    lc2 = Lc * Lc
+    lc3 = Lc * lc2  # Lc^3
+    lc4 = lc2 * lc2  # Lc^4
+    lc5 = lc2 * lc3   # Lc^5
+    lc6 = lc3 * lc3  # Lc^6
+    lc9 = lc6 * lc3  # Lc^9
+    lc10 = Lc * lc9   # Lc^10
+    lc11 = Lc * lc10   # Lc^11
+    lc12 = lc6 * lc6  # Lc^12
+    x4 = 2.0 * lc2
     x5 = St * x4
     x6 = 1.0 / Lp
-    x7 = 1.0 / x3
+    x7 = 1.0 / lc2
     x8 = (1.0/3.0) * x7
     x9 = x6 * x8
-    x10 = Lp * Lp
-    x38 = Lp * x10   # Lp^3
-    x28 = x10 * x10  # Lp^4
-    x14 = Lp * x28   # Lp^5
-    x11 = 1.0 / x10
+    lp2 = Lp * Lp
+    lp3 = Lp * lp2   # Lp^3
+    lp4 = lp2 * lp2  # Lp^4
+    lp5 = Lp * lp4   # Lp^5
+    x11 = 1.0 / lp2
     x12 = Lp * x2 - Lp * x5
     x13 = np.sqrt(3)
-    x46 = St * St
-    x39 = St * x46   # St ** 3
-    x25 = x46 * x46  # St ** 4
-    x16 = x46 * x39  # St ** 5
-    x17 = x15 * x16
-    x18 = x14 * x17
+    st2 = St * St
+    st3 = St * st2   # St ** 3
+    st4 = st2 * st2  # St ** 4
+    st5 = st2 * st3  # St ** 5
+    lc12st5 = lc12 * st5
+    lp5lc12st5 = lp5 * lc12st5
     x19 = 16.0 * kT
-    x21 = x14 * x16
-    x22 = x20 * x21
-    x23 = kT * x22
-    x24 = kT * kT
-    x26 = x15 * x25
-    x27 = x24 * x26
-    x29 = 27.0 * x28
-    x35 = d * d
-    x30 = d * x35  # d^3
-    x32 = x21 * x31
-    x34 = x21 * x33
-    x36 = kT * x35
-    x37 = np.sqrt(-48.0 * d * x23 + x18 * x19 - x19 * x30 * x32 + x27 * x29 + 48.0 * x34 * x36)
-    x40 = x38 * x39
-    x42 = 8.0 * x41
-    x43 = x35 * x40
-    x45 = 24.0 * x44
-    x47 = x10 * x46
-    x48 = x41 * x47
-    x50 = 8.0 * x30
-    x51 = x49 * x50
-    x53 = x40 * x52
+    lp5st5 = lp5 * st5
+    lc11lp5st5 = lc11 * lp5st5
+    ktlc11lp5st5 = kT * lc11lp5st5
+    kt2 = kT * kT
+    lc12st4 = lc12 * st4
+    kt2lc12st4 = kt2 * lc12st4
+    lp4_27 = 27.0 * lp4
+    d2 = d * d
+    d3 = d * d2  # d^3
+    lp5st5lc9 = lp5st5 * lc9
+    lp5st5lc10 = lp5st5 * lc10
+    ktd2 = kT * d2
+    lpst = Lp * St
+    ktlc3lpst = kT * lc3 * lpst
+    lplc4st = lc4 * lpst
+    lpstlc = Lc * lpst
+    kt2lc4 = kT * kT * lc4
+    lpstlc2 = lc2 * lpst
+    x37 = lc4 * st2 * lp2 * np.sqrt(-48.0 * d * ktlc3lpst + lplc4st * x19 - x19 * d3 * lpstlc + kt2lc4 * 27.0 + 48.0 * lpstlc2 * ktd2)
+    x40 = lp3 * st3
+    x42 = 8.0 * lc6
+    x43 = d2 * x40
+    x45 = 24.0 * lc4
+    x47 = lp2 * st2
+    x48 = lc6 * x47
+    x50 = 8.0 * d3
+    x51 = lc3 * x50
+    x53 = x40 * lc5
     x54 = 24.0 * d
     x55 = 27.0 * kT * x48 + x40 * x42 - x40 * x51 + x43 * x45 - x53 * x54
     x56 = 3.0 * x13 * x37 + x55
     x57 = x56 ** (1.0/3.0)
     x58 = x11 * x7
-    x59 = x44 * x46
+    x59 = lc4 * st2
     x60 = 32.0 * Lp
-    x61 = Lp * x46
-    x62 = 64.0 * d * x49
-    x63 = x3 * x35
-    x64 = x46 * x63
+    x61 = Lp * st2
+    x62 = 64.0 * d * lc3
+    x63 = lc2 * d2
+    x64 = st2 * x63
     x65 = 3.0 * np.sqrt(3.0) * x37 + x55
     x66 = x65 ** (-(1.0/3.0))
     x67 = (1.0/24.0) * x66
     x68 = x6 * x7
     x69 = x67 * x68
-    x70 = 16.0 * x10
-    x71 = x47 * x49
+    x70 = 16.0 * lp2
+    x71 = x47 * lc3
     x72 = 32.0 * d * x71 - x59 * x70 - x64 * x70
-    x73 = 18.0 * kT * x41
+    x73 = 18.0 * kT * lc6
     x74 = x61 * x73
-    x75 = x10 * x39
+    x75 = lp2 * st3
     x76 = x42 * x75
-    x77 = x52 * x54
+    x77 = lc5 * x54
     x78 = x75 * x77
     x79 = x51 * x75
-    x80 = x35 * x45 * x75
+    x80 = d2 * x45 * x75
     x81 = 40.0 * kT
-    x82 = x28 * x81
-    x83 = 120.0 * x16 * x28
+    x82 = lp4 * x81
+    x83 = 120.0 * st5 * lp4
     x84 = d * kT
-    x85 = x30 * x31
-    x86 = x33 * x36
-    x87 = -x16 * x82 * x85 + x17 * x82 - x20 * x83 * x84 + 54.0 * x27 * x38 + x83 * x86
+    x85 = d3 * lc9
+    x86 = lc10 * ktd2
+    x87 = -st5 * x82 * x85 + lc12st5 * x82 - lc11 * x83 * x84 + 54.0 * kt2lc12st4 * lp3 + x83 * x86
     x88 = 1.0 / x37
     x89 = 1.0 * x13 * x88
     x90 = (1.0/6.0) * x56 ** (-(2.0/3.0)) * x68
     x91 = np.sqrt(3) * x88
     x92 = (1.0/24.0) * x65 ** (-(4.0/3.0)) * x68 * x72
     x93 = Lp * x1
-    x94 = x6 / x49
+    x94 = x6 / lc3
     x95 = 16.0 * x53
-    x96 = 40.0 * d * x40 * x44
-    x97 = 54.0 * kT * x47 * x52
-    x98 = x3 * x40 * x50
-    x99 = 32.0 * x43 * x49
-    x100 = x24 * x28
-    x101 = x20 * x25
-    x102 = -72.0 * Lc ** 8 * kT * x21 * x30 + 162.0 * x100 * x101 + 96.0 * x23 + 240.0 * x32 * x36 - 264.0 * x34 * x84
-    x103 = St * x10
+    x96 = 40.0 * d * x40 * lc4
+    x97 = 54.0 * kT * x47 * lc5
+    x98 = lc2 * x40 * x50
+    x99 = 32.0 * x43 * lc3
+    x100 = kt2 * lp4
+    x101 = lc11 * st4
+    x102 = -72.0 * Lc ** 8 * kT * lp5st5 * d3 + 162.0 * x100 * x101 + 96.0 * ktlc11lp5st5 + 240.0 * lp5st5lc9 * ktd2 - 264.0 * lp5st5lc10 * x84
+    x103 = St * lp2
     x104 = 32.0 * x103
     x105 = x103 * x73
-    x106 = x38 * x46
+    x106 = lp3 * st2
     x107 = x106 * x42
     x108 = x106 * x77
     x109 = x106 * x51
-    x110 = 24.0 * x35
-    x111 = x110 * x38 * x59
-    x112 = x14 * x81
-    x113 = 120.0 * x14
-    x114 = 54.0 * x100 * x15 * x39 - x101 * x113 * x84 - x112 * x25 * x85 + x112 * x26 + x113 * x25 * x86
+    x110 = 24.0 * d2
+    x111 = x110 * lp3 * x59
+    x112 = lp5 * x81
+    x113 = 120.0 * lp5
+    x114 = 54.0 * x100 * lc12 * st3 - x101 * x113 * x84 - x112 * st4 * x85 + x112 * lc12st4 + x113 * st4 * x86
     x115 = 9.0 * x48
-    x116 = kT * x26 * x29 + x110 * x34 + 8.0 * x18 - x22 * x54 - x32 * x50
+    x116 = kT * lc12st4 * lp4_27 + x110 * lp5st5lc10 + 8.0 * lp5lc12st5 - lc11lp5st5 * x54 - lp5st5lc9 * x50
 
     return [-x11 * x12 * x8 - (1.0/6.0) * x57 * x58 + x58 * x67 * x72 -
             x69 * (-x59 * x60 - x60 * x64 + x61 * x62) +
             x9 * (x2 - x5) + x90 * (x74 + x76 - x78 - x79 + x80 + x87 * x89) -
             x92 * (-x74 - x76 + x78 + x79 - x80 - x87 * x91),
             -(2.0/3.0) * x12 * x94 - (1.0/3.0) * x57 * x94 + (1.0/12.0) * x66 * x72 * x94 -
-            x69 * (-32.0 * Lc * x35 * x47 + 96.0 * d * x3 * x47 - 64.0 * x71) +
+            x69 * (-32.0 * Lc * d2 * x47 + 96.0 * d * lc2 * x47 - 64.0 * x71) +
             x9 * (-4.0 * Lp * x0 + St * x93) + x90 * (x102 * x89 + x95 - x96 + x97 - x98 + x99) -
             x92 * (-x102 * x91 - x95 + x96 - x97 + x98 - x99),
-            -x69 * (x103 * x62 - x104 * x44 - x104 * x63) + x9 * (Lc * x93 - Lp * x4) +
+            -x69 * (x103 * x62 - x104 * lc4 - x104 * x63) + x9 * (Lc * x93 - Lp * x4) +
             x90 * (x105 + x107 - x108 - x109 + x111 + x114 * x89) -
             x92 * (-x105 - x107 + x108 + x109 - x111 - x114 * x91),
             x90 * (x115 + x116 * x89) - x92 * (-x115 - x116 * x91)]
+
+
+def invWLC_derivative(d, Lp, Lc, St, kT = 4.11):
+    x4 = d ** 3
+    x6 = d * d
+    Lc4 = Lc**4
+    St2 = St * St
+    Lp2 = Lp * Lp
+    x9 = np.sqrt(
+        27.0 * kT * kT * Lc ** 4 -
+        48.0 * d * Lc ** 3 * Lp * St * kT +
+        Lc ** 4 * 16.0 * Lp * St * kT -
+        16.0 * Lp * St * kT * x4 * Lc +
+        Lp * St * kT * x6 * 48.0 * Lc * Lc)
+    x10 = Lc ** 6
+    x11 = Lp ** 3 * St ** 3
+    x12 = 8.0 * x11
+    x13 = Lc ** 4
+    x14 = 24.0 * x11
+    x15 = Lp2 * St2
+    x16 = Lc ** 3
+    x17 = x12 * x16
+    x18 = Lc ** 5
+    x19 = -d * x14 * x18 + 27.0 * kT * x10 * x15 + x10 * x12 + x13 * x14 * x6 - x17 * x4
+    x20 = x19 + np.sqrt(3)*3 * x9 * Lc4 * St2 * Lp2
+    x21 = 32.0 * x15
+    x22 = x16 * x21
+    x23 = Lc ** 2
+    x24 = 1.0 / (Lp * x23)
+    x25 = (1.0/24.0) * x24
+    x26 = np.sqrt(3)
+    x27 = x12 * x18
+    x28 = 16.0 * x13
+    x29 = d * x11 * x28
+    x30 = x17 * x6
+    x31 = (Lc**5) * (Lp**3) * (St**3) * (
+            d * kT * 48.0 * Lc -
+            24.0 * Lc ** 2 * kT -
+            24.0 * kT * x6) / x9
+
+    return -x20 ** (-4.0/3.0) * x25 * (d * x22 - 16.0 * x15 * x23 * x6 - x15 * x28) * (x27 - x29 + x30 - np.sqrt(3) * x31) - \
+           x20 ** (-1.0/3.0) * x25 * (-d * x21 * x23 + x22) + \
+           (1.0/6.0) * x24 * (x19 + 3.0 * x26 * (St2 * Lp2 * x9) * Lc4) ** (-2.0/3.0) * \
+           (1.0 * x26 * x31 - x27 + x29 - x30) + \
+           (2.0/3.0) * St / Lc
 
 
 def WLC_derivative(F, Lp, Lc, St, kT = 4.11):
