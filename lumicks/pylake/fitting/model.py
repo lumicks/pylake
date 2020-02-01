b63526887928418d2dd72c34ca830b76c20fa512
Joep Vanlier <joep.vanlier@gmail.com>
2020-02-01 13:50:18 +0100

    Fixup! messed up the call to call.

--- lumicks/pylake/fitting/model.py
+++ lumicks/pylake/fitting/model.py
@@ -297,7 +297,7 @@ class Model:
             Plot options
         """
         x = independent if independent else np.sort(data.x)
-        plt.plot(x, self._call(x, data.get_parameters(global_parameters)), **kwargs)
+        plt.plot(x, self(x, data.get_parameters(global_parameters)), **kwargs)
 
 
 class CompositeModel(Model):
