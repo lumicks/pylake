48bb2eb20dacd2a9021b00f8e827a19916afeaac
Joep Vanlier <joep.vanlier@gmail.com>
2020-03-26 10:02:19 +0100

    fdfit: separate parameter trace fits

--- lumicks/pylake/fitting/detail/parameter_trace.py
+++ lumicks/pylake/fitting/detail/parameter_trace.py
@@ -1,10 +1,9 @@
 from .utilities import parse_transformation
 import numpy as np
-import scipy as sp
 import scipy.optimize as optim
 
 
-def parameter_trace(model, parameters, inverted_parameter, independent, dependent):
+def parameter_trace(model, parameters, inverted_parameter, independent, dependent, **kwargs):
     """Invert a model with respect to one parameter. This function fits a unique parameter for every data point in
     this data-set while keeping all other parameters fixed. This can be used to for example invert the model with
     respect to the contour length or some other parameter.
@@ -21,6 +20,8 @@ def parameter_trace(model, parameters, inverted_parameter, independent, dependen
         vector of values for the independent variable
     dependent: array_like
         vector of values for the dependent variable
+    **kwargs:
+        forwarded to scipy.optimize.least_squares
 
     Examples
     --------
@@ -47,19 +48,19 @@ def parameter_trace(model, parameters, inverted_parameter, independent, dependen
     ub = parameters[inverted_parameter].ub
     inverted_parameter_index = parameter_names.index(inverted_parameter)
 
-    def residual(inverted_parameter_values):
-        parameter_vector[inverted_parameter_index] = inverted_parameter_values
-        return dependent - model._raw_call(independent, parameter_vector)
+    def fit_single_point(x, y):
+        def residual(inverted_parameter_value):
+            parameter_vector[inverted_parameter_index] = inverted_parameter_value
+            return y - model._raw_call(np.array([x]), parameter_vector)
 
-    def jacobian(inverted_parameter_values):
-        parameter_vector[inverted_parameter_index] = inverted_parameter_values
-        return -sp.sparse.diags(model.jacobian(independent, parameter_vector)[inverted_parameter_index, :], offsets=0)
+        def jacobian(inverted_parameter_value):
+            parameter_vector[inverted_parameter_index] = inverted_parameter_value
+            return -model.jacobian(np.array([x]), parameter_vector)[inverted_parameter_index, :]
 
-    initial_estimate = np.ones(independent.shape) * parameter_vector[inverted_parameter_index]
+        jac = jacobian if model.has_jacobian else "2-point"
+        result = optim.least_squares(residual, parameter_vector[inverted_parameter_index], jac=jac,
+                                     bounds=(lb, ub), method='trf', **kwargs)
 
-    jac = jacobian if model.has_jacobian else "2-point"
-    result = optim.least_squares(residual, initial_estimate, jac=jac,
-                                 jac_sparsity=sp.sparse.identity(len(independent)),
-                                 bounds=(lb, ub), method='trf', ftol=1e-08, xtol=1e-08, gtol=1e-8)
+        return result.x[0]
 
-    return result.x
+    return np.array([fit_single_point(x, y) for (x, y) in zip(independent, dependent)])
