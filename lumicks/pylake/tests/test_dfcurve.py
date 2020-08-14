54b5b3e32b9516fde06572266a9a602f766b38cc
Joep Vanlier <joep.vanlier@gmail.com>
2020-08-14 11:27:58 +0200

    fdcurve: Add slicing behaviour to fdcurve

--- lumicks/pylake/tests/test_dfcurve.py
+++ lumicks/pylake/tests/test_dfcurve.py
@@ -29,3 +29,15 @@ def test_subtraction():
     fd2 = make_mock_fd(force=[1, 1, 1], distance=[5, 6, 7], start=100)
     assert np.allclose((fd1 - fd2).f.data, [])
     assert np.allclose((fd2 - fd1).f.data, [])
+
+
+def test_slice():
+    fd = make_mock_fd(force=[1, 1, 1], distance=[5, 6, 7], start=100)
+    assert np.allclose(fd[101:].d.data, [6, 7])
+    assert np.allclose(fd[:101].d.data, [5])
+    assert np.allclose(fd[101:300].d.data, [6, 7])
+    assert np.allclose(fd[101:102].d.data, [6])
+
+    sliced = fd[101:102]
+    assert id(fd.f) != id(sliced.f)
+    assert id(fd.d) != id(sliced.d)
