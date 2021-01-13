04f413d5853d2f4162474ff9ebcc3f7f8c4c52f6
Ray Pauszek <r.pauszek@lumicks.com>
2021-01-13 15:42:37 +0100

    range_selector: refactor, add FdDistanceRangeSelectorWidget

--- lumicks/pylake/nb_widgets/range_selector.py
+++ lumicks/pylake/nb_widgets/range_selector.py
@@ -3,34 +3,53 @@ import numpy as np
 import time
 
 
-class SliceRangeSelector:
-    def __init__(self, channel_slice, axes=None, show=True):
+class BaseRangeSelectorWidget:
+    """ Base class for range selection widgets.
+        Takes care of setting up connections and internal event handling.
+
+        Parameters
+        __________
+        axes : matplotlib.axes.Axes
+            Axes instance used for plotting
+        show : bool
+            Controls if plot is rendered immediately
+        range_conversion_function : function
+            Function to convert from units of the underlying data to plotting units,
+            for example: timestamps -> seconds in the case of a Slice
+            default simply returns the value unchanged.
+    """
+    def __init__(self, axes=None, show=True, range_conversion_fcn=lambda point:point):
         self._axes = axes if axes else plt.axes(label=f"lk_slice_widget_{time.time()}")
-        self.slice = channel_slice
-        self.time = self.slice.timestamps
         self.connection_id = None
         self.current_range = []
         self._ranges = []
+        self.range_conversion_fcn = range_conversion_fcn
         self.open = show
 
         if show:
             self._axes.get_figure().canvas.mpl_connect('close_event', self.close)
             self.update_plot()
 
+    @property
+    def xdata(self):
+        raise NotImplementedError
+
+    @property
+    def ranges(self):
+        """Returns selected list of timestamp ranges"""
+        return np.copy(self._ranges)
+
     def close(self, event):
         self.open = False
 
     def wait_for_input(self):
         while self.open:
-            plt.pause(.0001)
+            plt.pause(0.0001)
 
-    def to_seconds(self, timestamp):
-        return (timestamp - self.time[0]) / 1e9
-
-    def _add_point(self, fd_timestamp):
+    def _add_point(self, point):
         """Adds a point intended to be a range endpoint. Once two points are selected the new range selection is
         committed and the range appended."""
-        self.current_range.append(fd_timestamp)
+        self.current_range.append(point)
 
         # We have a range selected. Append it to the list.
         if len(self.current_range) == 2:
@@ -39,12 +58,12 @@ class SliceRangeSelector:
             self.update_plot()
 
         # Draw a vertical line for some immediate visual feedback
-        self._axes.axvline(self.to_seconds(fd_timestamp))
+        self._axes.axvline(self.range_conversion_fcn(point))
         self._axes.get_figure().canvas.draw()
 
-    def _remove_range(self, fd_timestamp):
+    def _remove_range(self, point):
         """Removes a range if the provided timestamp falls inside it."""
-        in_range = [start < fd_timestamp < stop for start, stop in self._ranges]
+        in_range = [start < point < stop for start, stop in self._ranges]
         selected = np.nonzero(in_range)[0]
         if len(selected) > 0:
             selected_ranges = [self._ranges[x] for x in selected]
@@ -60,15 +79,14 @@ class SliceRangeSelector:
         if event.canvas.widgetlock.locked():
             return
 
-        event_timestamp = event.xdata * 1e9 + self.time[0]
         if event.button == 1:
             # Find the data point nearest to the event and determine its timestamp
-            fd_timestamp = self.time[np.argmin(np.abs(self.time - event_timestamp))]
-            if fd_timestamp:
-                self._add_point(fd_timestamp)
+            point = self.xdata[np.argmin(np.abs(self.xdata - event.xdata))]
+            if point:
+                self._add_point(point)
         elif event.button == 3:
             # Find whether we clicked an existing range
-            self._remove_range(event_timestamp)
+            self._remove_range(event.xdata)
 
     def connect_click_callback(self):
         """Connects a callback for clicking the axes"""
@@ -83,7 +101,7 @@ class SliceRangeSelector:
         self._axes.clear()
 
         for i, (t_start, t_end) in enumerate(self._ranges):
-            t_start, t_end = self.to_seconds(t_start), self.to_seconds(t_end)
+            t_start, t_end = self.range_conversion_fcn(t_start), self.range_conversion_fcn(t_end)
             self._axes.axvline(t_start)
             self._axes.axvline(t_end)
             self._axes.axvspan(t_start, t_end, alpha=0.15, color='blue')
@@ -91,17 +109,33 @@ class SliceRangeSelector:
 
         old_axis = plt.gca()
         plt.sca(self._axes)
-        self.slice.plot(linestyle='', marker='.', markersize=1)
+        self._plot_data()
         self.connect_click_callback()
-
-        plt.ylabel('Force [pN]')
-        plt.xlabel('Time [s]')
         plt.sca(old_axis)
 
+    def _plot_data(self):
+        raise NotImplementedError
+
+
+class SliceRangeSelectorWidget(BaseRangeSelectorWidget):
+    def __init__(self, channel_slice, axes=None, show=True):
+        self.slice = channel_slice
+        self.time = self.slice.timestamps
+        super().__init__(axes, show, self.to_seconds)
+
     @property
-    def ranges(self):
-        """Returns selected list of timestamp ranges"""
-        return np.copy(self._ranges)
+    def xdata(self):
+        return self.time
+
+    def to_seconds(self, timestamp):
+        return (timestamp - self.time[0]) / 1e9
+
+    def handle_button_event(self, event):
+        event.xdata = event.xdata * 1e9 + self.time[0] # convert seconds to timestamp
+        super().handle_button_event(event)
+
+    def _plot_data(self):
+        self.slice.plot(linestyle='', marker='.', markersize=1)
 
     @property
     def slices(self):
@@ -109,7 +143,7 @@ class SliceRangeSelector:
         return [self.slice[start:stop] for start, stop in self._ranges]
 
 
-class FdRangeSelectorWidget(SliceRangeSelector):
+class FdTimeRangeSelectorWidget(SliceRangeSelectorWidget):
     def __init__(self, fd_curve, axes=None, show=True):
         super().__init__(fd_curve.f, axes, show)
         self.fd_curve = fd_curve
@@ -120,8 +154,26 @@ class FdRangeSelectorWidget(SliceRangeSelector):
         return [self.fd_curve[start:stop] for start, stop in self._ranges]
 
 
+class FdDistanceRangeSelectorWidget(BaseRangeSelectorWidget):
+    def __init__(self, fd_curve, axes=None, show=True):
+        super().__init__(axes, show)
+        self.fd_curve = fd_curve        
+
+    @property
+    def xdata(self):
+        return self.fd_curve.d.data
+
+    def _plot_data(self):
+        self.fd_curve.plot_scatter(s=2)
+
+    @property
+    def fdcurves(self):
+        """Return list of selected fdcurves of data as `lumicks.pylake.FdCurve`"""
+        return [self.fd_curve._slice_by_distance(min_dist, max_dist) for min_dist, max_dist in self._ranges]
+
+
 class FdRangeSelector:
-    def __init__(self, fd_curves):
+    def __init__(self, fd_curves, select_by="time"):
         """Open widget to select regions in multiple F,d curves
 
         Parameters
@@ -141,10 +193,17 @@ class FdRangeSelector:
                                 "works. Please note that you may have to restart the notebook kernel for this to "
                                 "work."))
 
+        if select_by == "time":
+            widget = FdTimeRangeSelectorWidget
+        elif select_by == "distance":
+            widget = FdDistanceRangeSelectorWidget
+        else:
+            raise ValueError(f"selection by '{select_by}' is not recognized.")
+
         plt.figure()
         self.axes = plt.axes()
         self.active_plot = None
-        self.selectors = {key: FdRangeSelectorWidget(curve, self.axes, show=False) for key, curve in fd_curves.items()}
+        self.selectors = {key: widget(curve, self.axes, show=False) for key, curve in fd_curves.items()}
         keys = [key for key, curve in fd_curves.items()]
         ipywidgets.interact(self.update_plot, curve=ipywidgets.Dropdown(options=keys))
 
