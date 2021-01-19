cf1be091b9b11794556292c07fd4a930f60bba1d
Joep Vanlier <joep.vanlier@gmail.com>
2021-01-19 18:53:04 +0100

    fd_ensemble: add basic f,d alignment procedure

--- lumicks/pylake/fdensemble.py
+++ lumicks/pylake/fdensemble.py
@@ -66,3 +66,21 @@ class FdEnsemble:
     def d(self):
         return np.hstack([fd.d.data for fd in self.values()])
 
+    def align_linear(self, distance_range_low, distance_range_high):
+        """Aligns F,d curves to the first F,d curve in the ensemble.
+
+        Force is aligned by taking the mean of the lowest distances. Distance is aligned by considering the last segment
+        of each F,d curve. This method regresses a line to the last segment of each F,d curve and aligns the curves
+        based on this regressed line. Note that this requires the ends of the aligned F,d curves to be in a comparably
+        folded state and obtained in the elastic range of the force, distance curve. If any of these assumptions are not
+        met, this method should not be applied.
+
+        Parameters
+        ----------
+        distance_range_low : float
+            Range of distances to use for the force alignment. Distances in the range [smallest_distance,
+            smallest_distance + distance_range_low) are used to determine the force offsets.
+        distance_range_high : float
+            Upper range of distances to use. Distances in the range [largest_distance - distance_range_high,
+            largest_distance] are used for the distance alignment."""
+        self.fd_curves_processed = align_fd_simple(self.fd_curves, distance_range_low, distance_range_high)
--- lumicks/pylake/tests/test_fdensemble.py
+++ lumicks/pylake/tests/test_fdensemble.py
@@ -1,4 +1,3 @@
-import pytest
 import numpy as np
 from lumicks.pylake.detail.alignment import align_force_simple, align_distance_simple, align_fd_simple
 from lumicks.pylake.fdcurve import FDCurve
@@ -14,6 +13,94 @@ def make_mock_fd(force, distance, start=0):
     fd._force_cache = Slice(TimeSeries(force, timestamps))
     fd._distance_cache = Slice(TimeSeries(distance, timestamps))
     return fd
+
+
+def test_align_force_simple():
+    """Test whether alignment of curves with a constant offset in force produces aligned curves"""
+
+    force = np.hstack((np.ones(50), np.arange(1, 50)))
+    distance = np.arange(1, 100)
+    fd1 = make_mock_fd(force=force, distance=distance, start=0)
+    fd2 = make_mock_fd(force=force + 5, distance=distance, start=0)
+    fd3 = make_mock_fd(force=force + 15, distance=distance, start=0)
+    aligned = align_force_simple({"fd1": fd1, "fd2": fd2, "fd3": fd3}, distance_range=50)
+
+    for fd in aligned.values():
+        assert np.allclose(fd.d.data, distance)
+        assert np.allclose(fd.f.data, force)
+
+
+def test_align_distance_simple():
+    """Test whether alignment of curves with a constant offset in distance produces aligned curves"""
+
+    force = np.hstack((np.ones(50), np.arange(1, 52)))
+    distance = np.arange(1.0, 102.0)
+    fd1 = make_mock_fd(force=force, distance=distance, start=0)
+    fd2 = make_mock_fd(force=force, distance=distance + 4.0, start=0)
+    fd3 = make_mock_fd(force=force, distance=distance + 2.0, start=0)
+    aligned = align_distance_simple({"fd1": fd1, "fd2": fd2, "fd3": fd3}, distance_range=50)
+
+    for fd in aligned.values():
+        assert np.allclose(fd.d.data, distance)
+        assert np.allclose(fd.f.data, force)
+
+
+def test_align_fd_simple():
+    """Test whether alignment of curves with a constant offset in both force and distance produces aligned curves"""
+
+    force = np.hstack((np.ones(50), np.arange(1, 52)))
+    distance = np.arange(1.0, 102.0)
+    fd1 = make_mock_fd(force=force, distance=distance, start=0)
+    fd2 = make_mock_fd(force=force + 5.0, distance=distance + 4.0, start=0)
+    fd3 = make_mock_fd(force=force + 15.0, distance=distance + 2.0, start=0)
+    aligned = align_fd_simple({"fd1": fd1, "fd2": fd2, "fd3": fd3}, 50, 50)
+
+    for fd in aligned.values():
+        assert np.allclose(fd.f.data, force)
+        assert np.allclose(fd.d.data, distance)
+
+
+def test_non_constant_rate_fd_alignment_simple():
+    """Tests what happens when we try to align an Fd curve with a non-constant pulling rate. A non-constant pulling rate
+    would lead to a non-linear relation when just looking at either force or distance, rather than looking at them
+    at the same time. Here we test whether the alignment actually operates on f,d rather than on channels
+    independently"""
+
+    distance = np.hstack((np.arange(150) * np.arange(150)) / 2500)
+    force = np.copy(distance)
+    force[force < 2] = 2
+
+    def generate_data(num_shortened):
+        return make_mock_fd(force=force[:-num_shortened], distance=distance[:-num_shortened], start=0)
+
+    shortening = [10, 20, 33, 48, 14, 60]
+    fds = {f"f_{idx}": generate_data(num_shortened) for idx, num_shortened in enumerate(shortening)}
+    aligned = align_fd_simple(fds, 1.5, 1.2)
+
+    for fd, num_shortened in zip(aligned.values(), shortening):
+        assert np.allclose(fd.f.data, force[:-num_shortened])
+        assert np.allclose(fd.d.data, distance[:-num_shortened])
+
+
+def test_back_and_forth_rate_fd_alignment_simple():
+    """Test what happens when we align an F,d curve that keeps going back and forth (non monotonic distance). The reason
+    we want to test this specific case is because there may be F,d curves that go back and forth. If the code merely
+    takes the last and first N samples, then this test would fail."""
+    
+    distance = 2.0 + np.sin(np.arange(1000))
+    force = np.copy(distance)
+    force[force < 2.0] = 2
+
+    def generate_data(num_shortened):
+        return make_mock_fd(force=force[:-num_shortened], distance=distance[:-num_shortened], start=0)
+
+    shortening = [100, 200, 330]
+    fds = {f"fd_{idx}": generate_data(num_shortened) for idx, num_shortened in enumerate(shortening)}
+    aligned = align_fd_simple(fds, 1, 1)
+
+    for fd, num_shortened in zip(aligned.values(), shortening):
+        assert np.allclose(fd.f.data, force[:-num_shortened])
+        assert np.allclose(fd.d.data, distance[:-num_shortened])
 
 
 def test_iteration_fd_ensemble():
@@ -37,8 +124,11 @@ def test_iteration_fd_ensemble():
     for fd_a, fd_b in zip(fds.values(), fd_ensemble.values()):
         assert id(fd_a) == id(fd_b)
 
-    for fd_a, fd_b in zip(fds.values(), fd_ensemble.raw.values()):
-        assert id(fd_a) == id(fd_b)
+    # Test that alignment leads to new objects and that the originals weren't modified
+    fd_ensemble.align_linear(1, 1)
+    for fd_a, fd_new, fd_unchanged in zip(fds.values(), fd_ensemble.values(), fd_ensemble.raw.values()):
+        assert id(fd_a) != id(fd_new)
+        assert id(fd_a) == id(fd_unchanged)
 
 
 def test_fd_ensemble_accessors():
@@ -53,3 +143,6 @@ def test_fd_ensemble_accessors():
 
     assert np.allclose(fd_ensemble.f, np.array([0, 1, 2, 5, 6, 7, 15, 16, 17]))
     assert np.allclose(fd_ensemble.d, np.array([0, 1, 2, 0, 1, 2, 0, 1, 2]))
+
+    fd_ensemble.align_linear(20, 20)
+    assert np.allclose(fd_ensemble.f, np.array([0, 1, 2, 0, 1, 2, 0, 1, 2]))
