b0ce0d05094bb5c4f11334237aecc21b84c2d926
Joep Vanlier <joep.vanlier@gmail.com>
2021-03-18 18:57:01 +0100

    msd: verify that we have a sufficient number of points

--- lumicks/pylake/kymotracker/detail/msd_estimation.py
+++ lumicks/pylake/kymotracker/detail/msd_estimation.py
@@ -121,6 +121,10 @@ def optimal_points(localization_error, num_points):
     1) Michalet, X., & Berglund, A. J. (2012). Optimal diffusion coefficient estimation in
     single-particle tracking. Physical Review E, 85(6), 061916.
     """
+    if num_points < 4:
+        raise RuntimeError("You need at least 5 time points to estimate the number of points to "
+                           "include in the fit.")
+
     fa = 2.0 + 1.6 * localization_error ** 0.51
     limit_a = 3 + (4.5 * num_points ** 0.4 - 8.5) ** 1.2
 
