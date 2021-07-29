3c2ca376a7b86e3cbc28334234267303cff8ea4a
Joep Vanlier <joep.vanlier@gmail.com>
2021-07-29 10:09:44 +0200

    release: readying for release v0.9

--- lumicks/pylake/__about__.py
+++ lumicks/pylake/__about__.py
@@ -1,9 +1,9 @@
 __title__ = "lumicks.pylake"
-__version__ = "0.8.2"
+__version__ = "0.9.0"
 __summary__ = "Bluelake data analysis tools"
 __url__ = "https://github.com/lumicks/pylake"
 
 __author__ = "Lumicks B.V."
-__copyright__ = "2020, " + __author__
+__copyright__ = "2021, " + __author__
 __email__ = "devteam@lumicks.com"
 __license__ = "Apache 2.0"
