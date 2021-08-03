db692e26b2919518cdc9a474395d8dc2e38317f7
Ray Pauszek <r.pauszek@lumicks.com>
2021-08-03 14:47:39 +0200

    kymotracker: add refine_lines_gaussian

---
+++ lumicks/pylake/kymotracker/tests/conftest.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pytest
+from .data.generate_gaussian_data import read_dataset
+from lumicks.pylake.kymotracker.detail.calibrated_images import CalibratedKymographChannel
+from lumicks.pylake.kymotracker.kymoline import KymoLine, KymoLineGroup
+
+@pytest.fixture
+def gaussian_1d():
+    return read_dataset("gaussian_data_1d.npz")
+
+
+@pytest.fixture
+def kymogroups_2lines():
+    _, _, photon_count, parameters = read_dataset("kymo_data_2lines.npz")
+    pixel_size = parameters[0].pixel_size
+    centers = [p.center / pixel_size for p in parameters]
+
+    image = CalibratedKymographChannel.from_array(photon_count, pixel_size=pixel_size)
+    _, n_frames = image.data.shape
+
+    lines = KymoLineGroup([KymoLine(np.arange(n_frames), np.full(n_frames, c), image) for c in centers])
+
+    # introduce gaps into tracked lines
+    use_frames = np.array([0, 1, -2, -1])
+    gapped_lines = KymoLineGroup([KymoLine(line.time_idx[use_frames], line.coordinate_idx[use_frames], image)
+                                  for line in lines])
+
+    # crop the ends of initial lines and make new set of lines with one cropped and the second full
+    truncated_lines = KymoLineGroup([KymoLine(np.arange(1, n_frames-2), np.full(n_frames-3, c), image) for c in centers])
+    mixed_lines = KymoLineGroup([truncated_lines[0], lines[1]])
+
+    return lines, gapped_lines, mixed_lines
--- lumicks/pylake/kymotracker/tests/test_refinement.py
+++ lumicks/pylake/kymotracker/tests/test_refinement.py
@@ -1,5 +1,5 @@
 from lumicks.pylake.kymotracker.detail.calibrated_images import CalibratedKymographChannel
-from lumicks.pylake.kymotracker.kymotracker import refine_lines_centroid
+from lumicks.pylake.kymotracker.kymotracker import refine_lines_centroid, refine_lines_gaussian
 from lumicks.pylake.kymotracker.kymoline import KymoLine
 import numpy as np
 import pytest
@@ -54,3 +54,57 @@ def test_refinement_line(loc, inv_sigma=0.3):
     calibrated_image = CalibratedKymographChannel.from_array(np.expand_dims(image, 1))
     line = refine_lines_centroid([KymoLine([0], [25], image=calibrated_image)], 5)[0]
     np.testing.assert_allclose(line.coordinate_idx, loc, rtol=1e-2)
+
+
+def test_gaussian_refinement(kymogroups_2lines):
+    lines, gapped_lines, mixed_lines = kymogroups_2lines
+
+    # full data, no overlap
+    refined = refine_lines_gaussian(lines, window=3, refine_missing_frames=True,
+                                    overlap_strategy="ignore")
+    assert np.allclose(refined[0].position, [3.54449334, 3.52869383, 3.51225048, 3.38714712, 3.48588412])
+    assert np.allclose(refined[1].position, [4.96700319, 4.99771575, 5.04086914, 5.0066495,  4.99092852])
+
+    # initial guess for sigma
+    refined = refine_lines_gaussian(lines, window=3, refine_missing_frames=True,
+                                    overlap_strategy="ignore", initial_sigma=0.250)
+    assert np.allclose(refined[0].position, [3.54796251, 3.52869378, 3.51225141, 3.4718899, 3.48588423])
+    assert np.allclose(refined[1].position, [4.96700218, 4.99771571, 5.04086917, 5.00664717, 4.9909296])
+
+    # all frames overlap, therefore skipped and result is empty
+    with pytest.warns(UserWarning):
+        refined = refine_lines_gaussian(lines, window=10, refine_missing_frames=True,
+                                        overlap_strategy="skip")
+    assert len(refined) == 0
+
+    # invalid overlap strategy
+    with pytest.raises(AssertionError):
+        refined = refine_lines_gaussian(lines, window=3, refine_missing_frames=True,
+                                        overlap_strategy="something")
+
+    # gapped data, fill in missing frames
+    refined = refine_lines_gaussian(gapped_lines, window=3, refine_missing_frames=True,
+                                    overlap_strategy="skip")
+    assert np.allclose(refined[0].position, [3.54449334, 3.52869383, 3.51225048, 3.38714712, 3.48588412])
+    assert np.allclose(refined[1].position, [4.96700319, 4.99771575, 5.04086914, 5.0066495,  4.99092852])
+
+    # gapped data, skip missing frames
+    refined = refine_lines_gaussian(gapped_lines, window=3, refine_missing_frames=False,
+                                    overlap_strategy="ignore")
+    assert np.allclose(refined[0].position, [3.54449334, 3.52869383, 3.38714712, 3.48588412])
+    assert np.allclose(refined[1].position, [4.96700319, 4.99771575, 5.0066495,  4.99092852])
+
+    # mixed length lines, no overlap
+    refined = refine_lines_gaussian(mixed_lines, window=3, refine_missing_frames=True,
+                                    overlap_strategy="skip")
+    assert np.allclose(refined[0].position, [3.52869383, 3.51225048])
+    assert np.allclose(refined[1].position, [4.96700319, 4.99771575, 5.04086914, 5.0066495, 4.99092852])
+
+    # mixed length lines, track windows overlap
+    with pytest.warns(UserWarning):
+        refined = refine_lines_gaussian(mixed_lines, window=10, refine_missing_frames=True,
+                                        overlap_strategy="skip")
+    # all frames in mixed_lines[0] overlap with second track, all skipped
+    assert len(refined) == 1
+    # 2 frames in mixed_lines[1] overlap with first track, 2 skipped, 3 fitted
+    assert np.allclose(refined[0].position, [4.94659924, 5.00920806, 4.97724526])
