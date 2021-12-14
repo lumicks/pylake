c01a9715434aba5e04e7ef370f56f998a1d2090b
Joep Vanlier <joep.vanlier@gmail.com>
2021-12-14 11:24:35 +0100

    fdfit: move tests into their own files

---
+++ lumicks/pylake/fitting/tests/test_stderr.py
@@ -0,0 +1,62 @@
+from lumicks.pylake.fitting.model import Model
+from lumicks.pylake.fitting.fit import Fit
+import pytest
+import numpy as np
+
+
+def test_no_jac():
+    x = np.arange(10)
+    y = np.array([8.24869073, 7.77648717, 11.9436565, 14.85406276, 22.73081526, 20.39692261, 32.48962353, 31.4775862,
+                  37.63807819, 40.50125925])
+
+    linear_model = Model("linear", lambda x, a, b: a * x + b)
+    linear_fit = Fit(linear_model)
+    linear_fit._add_data("test", x, y, {'linear/a': 5})
+    linear_fit.fit()
+
+    with pytest.raises(NotImplementedError):
+        linear_fit.cov
+
+
+def test_uncertainty_analysis():
+    x = np.arange(10)
+    y = np.array([8.24869073, 7.77648717, 11.9436565, 14.85406276, 22.73081526, 20.39692261, 32.48962353, 31.4775862,
+                  37.63807819, 40.50125925])
+
+    def quad(x, a=1, b=1, c=1):
+        return a * x * x + b * x + c
+
+    def quad_jac(x, a=1, b=1, c=1):
+        return np.vstack((x*x, x, np.ones(len(x))))
+
+    def linear(x, a=1, b=1):
+        f = a * x + b
+        return f
+
+    def linear_jac(x, a, b):
+        J = np.vstack((x, np.ones(len(x))))
+        return J
+
+    linear_model = Model("linear", linear, jacobian=linear_jac)
+    linear_fit = Fit(linear_model)
+    linear_fit._add_data("test", x, y)
+    linear_fit.fit()
+    model_quad = Model("quad", quad, jacobian=quad_jac)
+    quad_fit = Fit(model_quad)
+    quad_fit._add_data("test", x, y)
+    quad_fit.fit()
+
+    np.testing.assert_allclose(linear_fit.cov, np.array([[0.08524185, -0.38358832], [-0.38358832, 2.42939269]]))
+    np.testing.assert_allclose(quad_fit.cov, np.array([[0.01390294, -0.12512650,  0.16683533],
+                                               [-0.1251265,  1.21511735, -1.90192281],
+                                               [0.16683533, -1.90192281,  4.53792109]]), rtol=1e-6)
+
+    np.testing.assert_allclose(linear_fit.sigma[0], 2.65187717)
+    np.testing.assert_allclose(linear_fit.aic, 49.88412577726061)
+    np.testing.assert_allclose(linear_fit.aicc, 51.59841149154632)
+    np.testing.assert_allclose(linear_fit.bic, 50.4892959632487)
+
+    np.testing.assert_allclose(quad_fit.sigma[0], 2.70938272)
+    np.testing.assert_allclose(quad_fit.aic, 51.31318724618379)
+    np.testing.assert_allclose(quad_fit.aicc, 55.31318724618379)
+    np.testing.assert_allclose(quad_fit.bic, 52.220942525165924)
---
+++ lumicks/pylake/fitting/tests/test_utilities.py
@@ -0,0 +1,83 @@
+from collections import OrderedDict
+from lumicks.pylake.fitting.detail.utilities import (
+    parse_transformation,
+    unique_idx,
+    escape_tex,
+    latex_sqrt,
+)
+from lumicks.pylake.fitting.detail.model_implementation import (
+    solve_cubic_wlc,
+    invwlc_root_derivatives,
+)
+import numpy as np
+import pytest
+
+
+def test_unique_idx():
+    uiq, inv = unique_idx(['str', 'str', 'hmm', 'potato', 'hmm', 'str'])
+    assert(uiq == ['str', 'hmm', 'potato'])
+    assert(inv == [0, 0, 1, 2, 1, 0])
+
+
+def test_transformation_parser():
+    pars = ['blip', 'foo']
+    assert parse_transformation(pars, {'foo': 'new_foo'}) == OrderedDict((('blip', 'blip'), ('foo', 'new_foo')))
+    assert parse_transformation(pars, {'foo': 5}) == OrderedDict((('blip', 'blip'), ('foo', 5)))
+
+    with pytest.raises(KeyError):
+        parse_transformation(pars, {'blap': 'new_foo'}) == OrderedDict((('blip', 'blip'), ('foo', 'new_foo')))
+
+    param_names = ['gamma', 'alpha', 'beta', 'delta']
+    params = OrderedDict(zip(param_names, param_names))
+    post_params = parse_transformation(params, {'gamma': 'gamma_specific', 'beta': 'beta_specific'})
+    assert (post_params['gamma'] == 'gamma_specific')
+    assert (post_params['alpha'] == 'alpha')
+    assert (post_params['beta'] == 'beta_specific')
+    assert (post_params['delta'] == 'delta')
+
+    with pytest.raises(KeyError):
+        parse_transformation(params, {'doesnotexist': 'yep'})
+
+
+def test_tex_replacement():
+    assert escape_tex("DNA/Hi") == "Hi_{DNA}"
+    assert escape_tex("DNA/Hi_There") == "Hi\\_There_{DNA}"
+    assert escape_tex("DNA_model/Hi_There") == "Hi\\_There_{DNA\\_model}"
+    assert escape_tex("Hi_There") == "Hi\\_There"
+    assert latex_sqrt("test") == r"\sqrt{test}"
+
+
+def test_analytic_roots():
+    a = np.array([0.0])
+    b = np.array([-3.0])
+    c = np.array([1.0])
+
+    np.testing.assert_allclose(
+        np.sort(np.roots(np.hstack((np.array([1.0]), a, b, c)))),
+        np.sort(
+            np.array(
+                [
+                    solve_cubic_wlc(a, b, c, 0)[0],
+                    solve_cubic_wlc(a, b, c, 1)[0],
+                    solve_cubic_wlc(a, b, c, 2)[0],
+                ]
+            )
+        ),
+    )
+
+    with pytest.raises(RuntimeError):
+        solve_cubic_wlc(a, b, c, 3)
+
+    def test_root_derivatives(root):
+        dx = 1e-5
+        ref_root = solve_cubic_wlc(a, b, c, root)
+        da = (solve_cubic_wlc(a + dx, b, c, root) - ref_root) / dx
+        db = (solve_cubic_wlc(a, b + dx, c, root) - ref_root) / dx
+        dc = (solve_cubic_wlc(a, b, c + dx, root) - ref_root) / dx
+
+        np.testing.assert_allclose(np.array(invwlc_root_derivatives(a, b, c, root)), np.array([da, db, dc]), atol=1e-5,
+                           rtol=1e-5)
+
+    test_root_derivatives(0)
+    test_root_derivatives(1)
+    test_root_derivatives(2)
