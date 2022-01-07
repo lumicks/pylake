12f6cb3d036ab7b84d67f771865dc3a34dd342d5
Ray Pauszek <r.pauszek@lumicks.com>
2022-01-07 15:07:11 +0100

    tests: move CorrelatedStack tests to test_imaging_camera directory
    * split tests into multiple files
    * add conftest
    * run black

---
+++ lumicks/pylake/tests/test_imaging_camera/conftest.py
@@ -0,0 +1,74 @@
+import pytest
+from ..data.mock_widefield import make_alignment_image_data, write_tiff_file
+
+
+@pytest.fixture(scope="module")
+def tiff_dir(tmpdir_factory):
+    return tmpdir_factory.mktemp("tiffs")
+
+
+@pytest.fixture(scope="module")
+def spot_coordinates():
+    return [[20, 30], [50, 50], [120, 30], [50, 70], [150, 60]]
+
+
+@pytest.fixture(scope="module")
+def warp_parameters():
+    return {
+        "red_warp_parameters": {"Tx": 20, "Ty": 10, "theta": 3},
+        "blue_warp_parameters": {"Tx": 10, "Ty": 20, "theta": -3},
+    }
+
+
+@pytest.fixture(scope="module")
+def gray_alignment_image_data(spot_coordinates, warp_parameters):
+    return make_alignment_image_data(
+        spot_coordinates, version=1, bit_depth=8, camera="irm", **warp_parameters
+    )
+
+
+@pytest.fixture(scope="module", params=[1, 2])
+def rgb_alignment_image_data(spot_coordinates, warp_parameters, request):
+    return make_alignment_image_data(
+        spot_coordinates, version=request.param, bit_depth=16, camera="wt", **warp_parameters
+    )
+
+
+@pytest.fixture(scope="module", params=[1, 2])
+def rgb_alignment_image_data_offset(spot_coordinates, warp_parameters, request):
+    return make_alignment_image_data(
+        spot_coordinates,
+        version=request.param,
+        bit_depth=16,
+        offsets=(50, 50),
+        camera="wt",
+        **warp_parameters,
+    )
+
+
+@pytest.fixture(scope="module")
+def rgb_tiff_file(tiff_dir, rgb_alignment_image_data):
+    mock_filename = tiff_dir.join("rgb_single.tiff")
+    write_tiff_file(rgb_alignment_image_data, n_frames=1, filename=str(mock_filename))
+    return mock_filename
+
+
+@pytest.fixture(scope="module")
+def rgb_tiff_file_multi(tiff_dir, rgb_alignment_image_data):
+    mock_filename = tiff_dir.join("rgb_multi.tiff")
+    write_tiff_file(rgb_alignment_image_data, n_frames=2, filename=str(mock_filename))
+    return mock_filename
+
+
+@pytest.fixture(scope="module")
+def gray_tiff_file(tiff_dir, gray_alignment_image_data):
+    mock_filename = tiff_dir.join("gray_single.tiff")
+    write_tiff_file(gray_alignment_image_data, n_frames=1, filename=str(mock_filename))
+    return mock_filename
+
+
+@pytest.fixture(scope="module")
+def gray_tiff_file_multi(tiff_dir, gray_alignment_image_data):
+    mock_filename = tiff_dir.join("gray_multi.tiff")
+    write_tiff_file(gray_alignment_image_data, n_frames=2, filename=str(mock_filename))
+    return mock_filename
---
+++ lumicks/pylake/tests/test_imaging_camera/test_export.py
@@ -0,0 +1,54 @@
+import pytest
+import tifffile
+import itertools
+from lumicks.pylake import CorrelatedStack
+
+
+def test_export(rgb_tiff_file, rgb_tiff_file_multi, gray_tiff_file, gray_tiff_file_multi):
+    from os import stat
+
+    filenames = (rgb_tiff_file, rgb_tiff_file_multi, gray_tiff_file, gray_tiff_file_multi)
+    for filename, align in itertools.product(filenames, (True, False)):
+        savename = str(filename.new(purebasename=f"out_{filename.purebasename}"))
+        stack = CorrelatedStack(str(filename), align)
+        stack.export_tiff(savename)
+        stack.src._tiff_file.close()
+        assert stat(savename).st_size > 0
+
+        with tifffile.TiffFile(str(filename)) as tif0, tifffile.TiffFile(savename) as tif:
+            assert len(tif0.pages) == len(tif.pages)
+            assert tif0.pages[0].software != tif.pages[0].software
+            assert "pylake" in tif.pages[0].software
+            if stack._get_frame(0).is_rgb:
+                if stack._get_frame(0)._is_aligned:
+                    assert "Applied channel 0 alignment" in tif.pages[0].description
+                    assert "Channel 0 alignment" not in tif.pages[0].description
+                else:
+                    assert "Applied channel 0 alignment" not in tif.pages[0].description
+                    assert "Channel 0 alignment" in tif.pages[0].description
+            for page0, page in zip(tif0.pages, tif.pages):
+                assert page0.tags["DateTime"].value == page.tags["DateTime"].value
+
+
+def test_export_roi(rgb_tiff_file, rgb_tiff_file_multi, gray_tiff_file, gray_tiff_file_multi):
+    from os import stat
+
+    for filename in (rgb_tiff_file, rgb_tiff_file_multi, gray_tiff_file, gray_tiff_file_multi):
+        savename = str(filename.new(purebasename=f"roi_out_{filename.purebasename}"))
+        stack = CorrelatedStack(str(filename))
+        with pytest.warns(DeprecationWarning):
+            stack.export_tiff(savename, roi=[10, 190, 20, 80])
+        assert stat(savename).st_size > 0
+
+        with tifffile.TiffFile(savename) as tif:
+            assert tif.pages[0].tags["ImageWidth"].value == 180
+            assert tif.pages[0].tags["ImageLength"].value == 60
+
+        with pytest.raises(ValueError):
+            with pytest.warns(DeprecationWarning):
+                stack.export_tiff(savename, roi=[-10, 190, 20, 80])
+
+        with pytest.raises(ValueError):
+            with pytest.warns(DeprecationWarning):
+                stack.export_tiff(savename, roi=[190, 10, 20, 80])
+        stack.src._tiff_file.close()
