12ea38763ab43510c4aa7fcf263313aaa069ae19
Joep Vanlier <joep.vanlier@gmail.com>
2022-02-09 18:10:34 +0100

    touchdown: fit interference pattern
    In this commit we add the actual function that fits the sinewave interference pattern. This commit introduces some complexity for what seems to be a simple problem, so therefore this comment.
    
    Initially this code was simpler (and just fitted the entire function sine plus background using nonlinear optimization). It turned out however that this fit was not very reliable. It depended very strongly on the initial guess for both the phase and the frequency.
    
    The former, can be estimated in one iteration by recasting the problem a little bit (see the actual commit for that). If you rewrite the equation a little bit, you find that it is actually a linear problem in all but the frequency parameter. That's why in the objective function I first solve the linear system, and then only use nonlinear optimization for the frequency component. This approach is much faster and allows us to scan an entire range of frequencies at the cost of a single naive nonlinear optimization.
    
    What makes this even nicer for touchdown specifically is that we have a very narrow range of focal shifts that we consider reasonable for our systems (0.5 - 1.0), so that makes it easy to select a range over which to attempt optimizations.

--- lumicks/pylake/force_calibration/tests/test_touchdown.py
+++ lumicks/pylake/force_calibration/tests/test_touchdown.py
@@ -1,6 +1,9 @@
 import pytest
 import numpy as np
-from lumicks.pylake.force_calibration.touchdown import fit_piecewise_linear
+from lumicks.pylake.force_calibration.touchdown import (
+    fit_piecewise_linear,
+    fit_sine_with_polynomial,
+)
 
 
 @pytest.mark.parametrize(
@@ -26,3 +29,23 @@ def test_piecewise_linear_fit(direction, surface, slope1, slope2, offset):
 
     pars = fit_piecewise_linear(np.flip(independent), np.flip(y_func(independent)))
     np.testing.assert_allclose(pars, [surface, offset, slope1, slope2], atol=1e-12)
+
+
+@pytest.mark.parametrize(
+    "amplitude, frequency, phase_shift, poly_coeffs, f_guess",
+    [
+        [0.478463, 1.9889, 1.51936, [-1.26749, 8.06], 2.3],
+        [1.6, 1.3, 1.51936, [-1.26749, 8.06], 1.5],
+        [1.6, 1.3, 0.21936, [3.26749, -4.06], 1.5],
+        [0.6, 1.6, 0.21936, [3.26749, -4.06], 1.5],
+    ],
+)
+def test_sine_with_polynomial(amplitude, frequency, phase_shift, poly_coeffs, f_guess):
+    h = np.arange(2.5554, 0.0654, -0.01)
+    test_data = amplitude * np.sin(2.0 * np.pi * frequency * h + phase_shift) + np.polyval(
+        [-1.26749, 8.06], h
+    )
+
+    par, sim = fit_sine_with_polynomial(h, test_data, f_guess, [0.0, 5.0], background_degree=2)
+    np.testing.assert_allclose(frequency, par)
+    np.testing.assert_allclose(np.sum((sim - test_data) ** 2), 0, atol=1e-12)
--- lumicks/pylake/force_calibration/touchdown.py
+++ lumicks/pylake/force_calibration/touchdown.py
@@ -31,3 +31,50 @@ def fit_piecewise_linear(x, y):
     pars, _ = curve_fit(piecewise_linear, x, y, initial_guess)
 
     return pars
+
+
+def fit_sine_with_polynomial(independent, dependent, freq_guess, freq_bounds, background_degree):
+    """Fit a sine wave plus polynomial background.
+
+    We wish to fit a sine wave (with phase shift) plus a polynomial. By using a trick we can rewrite
+    this equation such that we only have to optimize over 1 variable:
+
+        amp * sin(a * x + phase) + other
+
+    can we written as:
+
+        amp * sin(phase) * cos(a * x) + amp * cos(phase) * sin(a * x) + other
+
+    By absorbing the sin and cosine into the amplitude, we change variables from amplitude and
+    phase, to sine amplitude and cosine amplitude. This means the problem of fitting a sine plus
+    polynomial is essentially a linear one in all variables except the frequency (our parameter of
+    interest). Doing this allows us to estimate this using only one estimated variable.
+
+    Parameters
+    ----------
+    independent : np.ndarray
+        Values for the independent variable
+    dependent : np.ndarray
+        Values for the dependent variable
+    freq_guess : float
+        Initial guess for the frequency
+    freq_bounds : 2-tuple of array_like
+        Bounds for the frequency guess
+    background_degree : int
+        Polynomial degree to use to fit the background
+    """
+
+    def sine_with_polynomial(x, frequency):
+        design_matrix = np.vstack(
+            (
+                np.sin(2.0 * np.pi * frequency * x),
+                np.cos(2.0 * np.pi * frequency * x),
+                x[np.newaxis, :] ** np.arange(0, background_degree + 1)[:, np.newaxis],
+            ),
+        )
+        ests, _, _, _ = np.linalg.lstsq(design_matrix.T, dependent, rcond=None)
+        return np.sum(design_matrix.T * ests, axis=1)
+
+    par, _ = curve_fit(sine_with_polynomial, independent, dependent, freq_guess, bounds=freq_bounds)
+    return par[0], sine_with_polynomial(independent, par[0])
+
