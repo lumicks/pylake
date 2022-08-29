0c2cb2a261d04544ef0d3da2a5ec6b65fd456ab2
Joep Vanlier <joep.vanlier@gmail.com>
2022-08-29 17:13:46 +0200

    adjustments: use class docstring for documentation

--- lumicks/pylake/adjustments.py
+++ lumicks/pylake/adjustments.py
@@ -3,55 +3,54 @@ import matplotlib as mpl
 
 
 class ColorAdjustment:
-    """Color adjustment for plotting"""
+    """Utility class to adjust the min/max values of image colormaps.
+
+    Values can be supplied as a single number or 1-component list, in which case the value is
+    applied to all color channels, or a 3-component list specifying the values for red, green,
+    and blue channels.
+
+    Parameters
+    ----------
+    minimum : array_like
+        1 or 3-component list of lower limits for the color mapping
+    maximum : array_like
+        1 or 3-component list of upper limits for the color mapping
+    mode : str
+        - "percentile" : color limits are given as percentiles of the image in question.
+          Percentiles are calculated for each color channel separately
+        - "absolute" : color limits are given as absolute values.
+
+        Note: When providing bounds in percentiles, limits will change depending on which image
+        you are looking at. When scrolling through a stack of images, the limits will not remain
+        constant.
+    gamma : float
+        1 or 3-component list of gamma adjustments.
+
+        Applies a power law to the data according to: `((data - vmin) / (vmax - vmin)) ** gamma`
+
+    Examples
+    --------
+    ::
+
+        from lumicks import pylake
+        file = pylake.File("example.h5")
+
+        # Plot scan with red color mapped from 5th to 95th percentile.
+        adjustment = lk.ColorAdjustment(5, 95, mode="percentile")
+        file.scans["scan"].plot(channel="red", adjustment=adjustment)
+
+        # Plot scan with RGB colors mapped from 5th to 95th percentile.
+        adjustment = lk.ColorAdjustment([5, 5, 5], [95, 95, 95], mode="percentile")
+        file.scans["scan"].plot(adjustment=adjustment)
+
+        stack = lk.CorrelatedStack("camera_recording.tiff")
+        # Plot force 1x with this stack and adjust the contrast by specifying an absolute upper
+        # and lower bound for the intensities.
+        absolute_adjustment = lk.ColorAdjustment([50, 30, 20], [1000, 195, 95])
+        stack.plot_correlated(file.force1x, channel="rgb", adjustment=absolute_adjustment)
+    """
 
     def __init__(self, minimum, maximum, mode="absolute", gamma=1):
-        """Utility class to adjust the min/max values of image colormaps.
-
-        Values can be supplied as a single number or 1-component list, in which case the value is
-        applied to all color channels, or a 3-component list specifying the values for red, green,
-        and blue channels.
-
-        Parameters
-        ----------
-        minimum : array_like
-            1 or 3-component list of lower limits for the color mapping
-        maximum : array_like
-            1 or 3-component list of upper limits for the color mapping
-        mode : str
-            - "percentile" : color limits are given as percentiles of the image in question.
-              Percentiles are calculated for each color channel separately
-            - "absolute" : color limits are given as absolute values.
-
-            Note: When providing bounds in percentiles, limits will change depending on which image
-            you are looking at. When scrolling through a stack of images, the limits will not remain
-            constant.
-        gamma : float
-            1 or 3-component list of gamma adjustments.
-
-            Applies a power law to the data according to: `((data - vmin) / (vmax - vmin)) ** gamma`
-
-        Examples
-        --------
-        ::
-
-            from lumicks import pylake
-            file = pylake.File("example.h5")
-
-            # Plot scan with red color mapped from 5th to 95th percentile.
-            adjustment = lk.ColorAdjustment(5, 95, mode="percentile")
-            file.scans["scan"].plot(channel="red", adjustment=adjustment)
-
-            # Plot scan with RGB colors mapped from 5th to 95th percentile.
-            adjustment = lk.ColorAdjustment([5, 5, 5], [95, 95, 95], mode="percentile")
-            file.scans["scan"].plot(adjustment=adjustment)
-
-            stack = lk.CorrelatedStack("camera_recording.tiff")
-            # Plot force 1x with this stack and adjust the contrast by specifying an absolute upper
-            # and lower bound for the intensities.
-            absolute_adjustment = lk.ColorAdjustment([50, 30, 20], [1000, 195, 95])
-            stack.plot_correlated(file.force1x, channel="rgb", adjustment=absolute_adjustment)
-        """
         minimum, maximum, gamma = (np.atleast_1d(x) for x in (minimum, maximum, gamma))
         self.mode = None if mode == "nothing" else mode
         if not self.mode:
