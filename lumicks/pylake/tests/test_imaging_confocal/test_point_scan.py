ecc0d88acb64625d41cc6b950e61b80a5410140d
Tobias Jachowski <t.jachowski@lumicks.com>
2022-11-08 14:54:38 +0100

    test_point_scan: update `test_point_scan`

--- lumicks/pylake/tests/test_imaging_confocal/test_point_scan.py
+++ lumicks/pylake/tests/test_imaging_confocal/test_point_scan.py
@@ -1,13 +1,52 @@
-import pytest
 import matplotlib.pyplot as plt
 import numpy as np
+import pytest
+
+from lumicks.pylake.point_scan import PointScan
+from ..data.mock_confocal import MockConfocalFile, generate_image_data
+
+start = np.int64(20e9)
+dt = np.int64(62.5e6)
+reference_image = np.random.poisson(10, (64, 1))
+reference_infowave, counts = generate_image_data(reference_image, 5, 3)
 
 
-def test_point_scans(test_point_scans, reference_timestamps, reference_counts):
+@pytest.fixture(scope="module")
+def reference_counts():
+    return counts[0]
+
+
+@pytest.fixture(scope="module")
+def reference_timestamps():
+    stop = start + len(reference_infowave) * dt
+    return np.arange(start, stop, 6.25e7).astype(np.int64)
+
+
+@pytest.fixture(scope="module")
+def test_point_scans(reference_counts):
+    point_scans = {}
+
+    mock_file, metadata, stop = MockConfocalFile.from_streams(
+        start,
+        dt,
+        [],
+        [],
+        [],
+        infowave=reference_infowave,
+        red_photon_counts=reference_counts,
+        green_photon_counts=reference_counts,
+        blue_photon_counts=reference_counts,
+    )
+    point_scans["PointScan1"] = PointScan("PointScan1", mock_file, start, stop, metadata)
+
+    return point_scans
+
+
+def test_point_scan_attrs(test_point_scans, reference_timestamps, reference_counts):
     ps = test_point_scans["PointScan1"]
     ps_red = ps.red_photon_count
 
-    assert ps_red.data.shape == (64,)
+    assert ps_red.data.shape == (64 * 5 + 2 * 3,)
 
     np.testing.assert_allclose(ps_red.timestamps, reference_timestamps)
     np.testing.assert_allclose(ps_red.data, reference_counts)
@@ -46,7 +85,7 @@ def test_deprecated_plotting(test_point_scans):
     with pytest.warns(
         DeprecationWarning,
         match=r"The call signature of `plot\(\)` has changed: Please, provide `axes` as a "
-        "keyword argument."
+        "keyword argument.",
     ):
         xline, yline = ps.plot("red", None)[0].get_xydata().T
 
@@ -56,7 +95,6 @@ def test_deprecated_plotting(test_point_scans):
         plt.close()
     # Test rejection of deprecated call with positional `axes` and double keyword assignment
     with pytest.raises(
-        TypeError,
-        match=r"`PointScan.plot\(\)` got multiple values for argument `axes`"
+        TypeError, match=r"`PointScan.plot\(\)` got multiple values for argument `axes`"
     ):
         ps.plot("rgb", None, axes=None)
