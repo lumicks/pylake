dac3d008ea64ed4e5e6bf1699d7b8d54ba4aae3f
JoepVanlier <joep.vanlier@gmail.com>
2023-07-11 17:28:49 +0200

    wavelets: add some basic convenience manipulations
    the product kernels are the "net" convolutions actually used in the wavelet transform

--- lumicks/pylake/kymotracker/detail/denoising.py
+++ lumicks/pylake/kymotracker/detail/denoising.py
@@ -2,6 +2,50 @@ import numpy as np
 import scipy.signal
 
 
+def equal_length(kernels):
+    """Make the kernels the same length (while keeping them centered)
+
+    Parameters
+    ----------
+    kernels : list of np.ndarray
+        List of kernels to symmetrize.
+
+    Raises
+    ------
+    ValueError
+        if any of the kernel isn't odd sized (violates the ability to center it).
+    """
+    target_length = len(kernels[-1])
+
+    def pad(kernel):
+        if len(kernel) % 2 == 0:
+            raise ValueError("This function should only be used with odd sized kernels")
+
+        padding = np.zeros(((target_length - len(kernel)) // 2, 1))
+        return np.vstack((padding, kernel, padding))
+
+    return [pad(k) for k in kernels]
+
+
+def generate_product_filters(kernels):
+    """Generate product filters
+
+    Generates a list of kernels that consist of the previous kernels convolved.
+
+    Parameters
+    ----------
+    kernels : list of np.ndarray
+        List of 1-dimensional kernels to calculate the convolutional product of.
+    """
+    current = np.asarray([[1.0]])
+    convolved_kernels = [current]
+    for k in kernels:
+        current = scipy.signal.convolve(current, k, mode="full")
+        convolved_kernels.append(current)
+
+    return convolved_kernels
+
+
 def generate_bspline_kernels(num_kernels):
     """Returns a list of à trous b3 spline kernels.
 
--- lumicks/pylake/kymotracker/tests/test_denoise.py
+++ lumicks/pylake/kymotracker/tests/test_denoise.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from lumicks.pylake.kymotracker.detail.denoising import generate_bspline_kernels
+from lumicks.pylake.kymotracker.detail.denoising import *
 
 
 def test_bspline_kernels_empty():
@@ -19,3 +19,30 @@ def test_bspline_kernels(num_kernels, kernel_index, reference_kernel):
     kernels = generate_bspline_kernels(num_kernels)
     np.testing.assert_allclose(kernels[kernel_index].T, [reference_kernel])
     assert len(kernels) == num_kernels
+
+
+def pad_kernels():
+    eq_kernels = equal_length([np.array([1]), np.array([1, 2, 3]), np.array([1, 2, 3, 4, 5])])
+    np.testing.assert_allclose(eq_kernels[0], [0, 0, 1, 0, 0])
+    np.testing.assert_allclose(eq_kernels[1], [0, 1, 2, 3, 0])
+    np.testing.assert_allclose(eq_kernels[2], [1, 2, 3, 4, 5])
+
+    for bad in (
+        [],
+        [[1, 2]],
+        [[1], [1, 2, 3], [1, 2, 3, 4]],
+    ):
+        with pytest.raises(
+            ValueError, match=r"This function should only be used with odd sized kernels"
+        ):
+            equal_length(bad)
+
+
+def test_product_filters():
+    prod_kernels = generate_product_filters(
+        [np.array([[1, 3, 5]]), np.array([[1, 2, 3]]), np.array([[1, 3, 5]])]
+    )
+    np.testing.assert_allclose(prod_kernels[0], [[1]])
+    np.testing.assert_allclose(prod_kernels[1], [[1, 3, 5]])
+    np.testing.assert_allclose(prod_kernels[2], [[1, 5, 14, 19, 15]])
+    np.testing.assert_allclose(prod_kernels[3], [[1, 8, 34, 86, 142, 140, 75]])
