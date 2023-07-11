63b8dcb2c651c13ef4f0c5e6fb44097bbdc1e302
JoepVanlier <joep.vanlier@gmail.com>
2023-07-11 17:38:25 +0200

    wavelets: calculate variance stabilizing transform
    Compute coefficients for the VST. These are used to Gaussian-ize the distribution of wavelet coefficients later.

--- lumicks/pylake/kymotracker/detail/denoising.py
+++ lumicks/pylake/kymotracker/detail/denoising.py
@@ -69,3 +69,111 @@ def generate_bspline_kernels(num_kernels):
         kernels.append(kernel.reshape((-1, 1)))
 
     return kernels
+
+
+def tau(kernel, power):
+    """Calculate filter summation from Sec 2. Paragraph 1."""
+    return np.sum(kernel**power)
+
+
+def calculate_vst_coefficients(kernel):
+    """Calculate optimal transform coefficients and expected standard deviation.
+
+    We calculate b and c for the following transform:
+
+        Z = b sign(Y + c) * sqrt(Y + c)
+
+    Using the coefficients `c` from Eq. 10 and `b` (which is defined above Eq. 12 in [3]_), we
+    estimate a transform such that Z - b sqrt(tau_1 lambda) ~ N(0, 1) where lambda is the Poisson
+    parameter [3]_.
+
+    Parameters
+    ----------
+    kernel : np.ndarray
+        Kernel at scale `j`. Note that this is the full kernel, so Kj * Kj-1 .. Kn
+        where * is convolution operator.
+
+    References
+    ----------
+    .. [3] Zhang, B., Fadili, J. M., & Starck, J. L. (2008). Wavelets, ridgelets, and curvelets
+           for Poisson noise removal. IEEE Transactions on image processing, 17(7), 1093-1108.
+    """
+    b = np.sign(tau(kernel, 1)) / np.sqrt(np.abs(tau(kernel, 1)))  # Defined above eqn 12
+    c = 7.0 * tau(kernel, 2) / (8.0 * tau(kernel, 1)) - tau(kernel, 3) / (2.0 * tau(kernel, 2))
+    return b, c
+
+
+def calculate_vst_stdev(kernel, prev_kernel):
+    """Calculate expected standard deviation of detail layer
+
+    Parameters
+    ----------
+    kernel, prev_kernel : np.ndarary
+        kernel at scale j and j - 1
+
+    Raises
+    ------
+    ValueError
+        if kernel sizes are not equal
+    """
+    if prev_kernel.shape != kernel.shape:
+        raise ValueError("Kernel shapes must be equal to calculate standard deviation")
+
+    term1 = tau(prev_kernel, 2) / (4 * tau(prev_kernel, 1) ** 2)
+    term2 = tau(kernel, 2) / (4 * tau(kernel, 1) ** 2)
+    term3 = np.sum(prev_kernel * kernel) / (2 * tau(prev_kernel, 1) * tau(kernel, 1))
+    expected_sd = np.sqrt(term1 + term2 - term3)
+
+    return expected_sd
+
+
+def variance_stabilizing_transform(image, coefficients):
+    b, c = coefficients
+    return b * np.sign(image + c) * np.sqrt(np.abs(image + c))
+
+
+def inverse_variance_stabilizing_transform(image, coefficients):
+    b, c = coefficients
+    return np.sign(image) * (image / b) ** 2 - c
+
+
+class MultiScaleVarianceStabilizingTransform:
+    """Calculates the required coefficients for determining the variance stabilizing transform (VST)
+
+    The VST is a generalization of the Anscombe transform which can convert Poisson distributed.
+    noise to approximately Gaussian distributed noise. For Poisson distributed data, the variance
+    scales with the mean. After the transform the distribution is roughly Gaussian with a variance
+    of 1. This transformation allows you to apply statistics that rely on normality post-transform.
+
+    By combining this with a wavelet procedure, the variance and bias of this procedure goes down
+    more quickly than when applied without any filters [1, 2].
+
+    Parameters
+    ----------
+    kernels : list of np.ndarray
+        List of kernels to use for the multiscale variance stabilizing transform.
+    two_dimensional : bool
+        Convert the kernel into a two-dimensional kernel.
+
+    References
+    ----------
+    .. [1] Zhang, B., Fadili, M. J., Starck, J. L., & Olivo-Marin, J. C. (2007, September).
+           Multiscale variance-stabilizing transform for mixed-Poisson-Gaussian processes and its
+           applications in bioimaging. In 2007 IEEE International Conference on Image Processing
+           (Vol. 6, pp. VI-233). IEEE.
+    .. [2] Zhang, B., Fadili, J. M., & Starck, J. L. (2008). Wavelets, ridgelets, and curvelets
+           for Poisson noise removal. IEEE Transactions on image processing, 17(7), 1093-1108.
+    """
+
+    def __init__(self, kernels, *, two_dimensional=False):
+        self._kernels = [k * k.T for k in kernels] if two_dimensional else kernels
+
+        # Full kernels contain h0 * h1 * ... * hn where * is convolution.
+        equal_length_kernels = equal_length(generate_product_filters(kernels))
+        self._full_kernels = [k * k.T if two_dimensional else k for k in equal_length_kernels]
+        self._coefficients = [calculate_vst_coefficients(k) for k in self._full_kernels]
+
+        self._stdev = [
+            calculate_vst_stdev(k1, k2)
+            for k1, k2 in zip(self._full_kernels[1:], self._full_kernels[:-1])
+        ]
--- lumicks/pylake/kymotracker/tests/test_denoise.py
+++ lumicks/pylake/kymotracker/tests/test_denoise.py
@@ -1,5 +1,4 @@
 import pytest
-import numpy as np
 from lumicks.pylake.kymotracker.detail.denoising import *
 
 
@@ -46,3 +45,57 @@ def test_product_filters():
     np.testing.assert_allclose(prod_kernels[1], [[1, 3, 5]])
     np.testing.assert_allclose(prod_kernels[2], [[1, 5, 14, 19, 15]])
     np.testing.assert_allclose(prod_kernels[3], [[1, 8, 34, 86, 142, 140, 75]])
+
+
+@pytest.mark.parametrize(
+    "kernel, coefficients",
+    [
+        ([1.0], (1.0, 3 / 8)),  # Anscombe transform's c
+        ([1.0, 0.0, 1.0], (1 / np.sqrt(2), 3 / 8)),
+        ([1.0, 2.0, 3.0], (0.4082482904638631, 0.7559523809523807)),
+        ([1.0, 3.0, 3.0, 7.0], (0.2672612419124244, 1.323529411764706)),
+    ],
+)
+def test_variance_stabilizing_coefficients(kernel, coefficients):
+    np.testing.assert_allclose(calculate_vst_coefficients(np.array(kernel)), coefficients)
+
+
+@pytest.mark.parametrize(
+    "kernel, prev_kernel, stdev",
+    [
+        ([1.0, 0.0, 1.0], [0.0, 1.0, 0.0], 0.6123724356957945),
+        ([1.0, 1.0, 2.0, 1.0, 2.0], [1.0, 0.0, 1.0, 0.0, 2.0], 0.15771001547014013),
+        ([[1.0, 0.0, 1.0], [1.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]], 0.25),
+    ],
+)
+def test_variance_stabilizing_stdev(kernel, prev_kernel, stdev):
+    np.testing.assert_allclose(
+        calculate_vst_stdev(np.array(kernel), np.array(prev_kernel)),
+        stdev,
+    )
+
+
+@pytest.mark.parametrize(
+    "kernel, prev_kernel",
+    [
+        ([1.0, 0.0, 1.0], [0.0, 1.0]),
+        ([[1.0, 0.0, 1.0], [1.0, 0.0, 1.0]], [1.0, 0.0]),
+    ],
+)
+def test_unequal_shape_vst_stdev(kernel, prev_kernel):
+    with pytest.raises(
+        ValueError, match=r"Kernel shapes must be equal to calculate standard deviation"
+    ):
+        calculate_vst_stdev(np.array(kernel), np.array(prev_kernel))
+
+
+def test_variance_stabilizing_transform():
+    img = np.array([[1, -1, 2, -2], [1, 2, 3, 4]])
+    coeffs = (2, 3 / 8)
+    ref = [
+        [2.34520788, -1.58113883, 3.082207, -2.54950976],
+        [2.34520788, 3.082207, 3.67423461, 4.18330013],
+    ]
+    result = variance_stabilizing_transform(img, coeffs)
+    np.testing.assert_allclose(result, ref)
+    np.testing.assert_allclose(inverse_variance_stabilizing_transform(result, coeffs), img)
