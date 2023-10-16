151fb602eaf1144cab56d7108a42542d601e6fab
rpauszek <ray.pauszek@gmail.com>
2023-10-16 17:43:23 +0200

    file_download: update to new Zenodo API

--- lumicks/pylake/tests/test_file_download.py
+++ lumicks/pylake/tests/test_file_download.py
@@ -10,7 +10,7 @@ from lumicks.pylake.file_download import (
 
 @pytest.mark.preflight
 def test_grab_record():
-    assert get_url_from_doi("10.5281/zenodo.4247279") == "https://zenodo.org/record/4247279"
+    assert get_url_from_doi("10.5281/zenodo.4247279") == "https://zenodo.org/records/4247279"
 
     with pytest.raises(RuntimeError, match="DOI could not be resolved"):
         assert get_url_from_doi("10.55281/zenodo.4247279")
@@ -21,8 +21,8 @@ def test_download_record_metadata():
     record = download_record_metadata("4280789")  # Older version of Pylake
 
     # Verify that the fields we rely on stay the same
-    assert record["files"][0]["checksum"] == "md5:1a401193ab22f0983f87855e2581075b"
-    assert record["files"][0]["key"] == "lumicks/pylake-v0.7.1.zip"
+    assert record["files"][0]["checksum"] == "1a401193ab22f0983f87855e2581075b"
+    assert record["files"][0]["filename"] == "lumicks/pylake-v0.7.1.zip"
     assert record["files"][0]["links"]["self"].startswith("https://zenodo.org/")  # Link may change
 
 
@@ -39,14 +39,14 @@ def test_download_from_doi(tmpdir_factory):
     files = download_from_doi("10.5281/zenodo.4247279", tmpdir, show_progress=False)
 
     # Validate checksum
-    assert verify_hash(files[0], *record["files"][0]["checksum"].split(":"))
+    assert verify_hash(files[0], record["files"][0]["checksum"])
 
     # Add a random character such that the checksum fails
     with open(files[0], "ab") as f:
         f.write(b"\x21")
 
     # Validate that the hash is no longer correct
-    assert not verify_hash(files[0], *record["files"][0]["checksum"].split(":"))
+    assert not verify_hash(files[0], record["files"][0]["checksum"])
 
     with pytest.raises(
         RuntimeError,
@@ -58,4 +58,4 @@ def test_download_from_doi(tmpdir_factory):
     download_from_doi("10.5281/zenodo.4247279", tmpdir, force_download=True, show_progress=False)
 
     # Validate checksum after forced re-download (should be OK again)
-    assert verify_hash(files[0], *record["files"][0]["checksum"].split(":"))
+    assert verify_hash(files[0], record["files"][0]["checksum"])
