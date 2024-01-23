60a8b36a0fb6616d47b877e45cc7bca5e4025446
Agostino Mirone <a.mirone@lumicks.com>
2024-01-23 18:01:27 +0100

    matplotlib

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         "h5py>=3.4, <4",
         "numpy>=1.24, <2",  # 1.24 is needed for dtype in vstack/hstack (Dec 18th, 2022)
         "scipy>=1.9, <2",  # 1.9.0 needed for lazy imports (July 29th, 2022)
-        "matplotlib>=3.5",
+        "matplotlib>=3.8",
         "tifffile>=2020.9.30",
         "tabulate>=0.8.8, <0.9",
         "cachetools>=3.1",
