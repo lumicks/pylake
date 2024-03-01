85f98859269ce6fb56dfdd3fd54173197d27a523
Joep Vanlier <joep.vanlier@gmail.com>
2024-03-01 12:35:28 +0100

    externalize state + online process noise estimator

--- lumicks/pylake/kymotracker/detail/kalman.py
+++ lumicks/pylake/kymotracker/detail/kalman.py
@@ -1,95 +1,130 @@
 import numpy as np
 
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class PredictionError:
+    m: float = 0
+    n: int = 0
+    M: float = 0
+
+    def with_update(self, kalman_update):
+        """Update the average prediction error"""
+
+        # Equation 3.19
+
+        # The kalman update is defined by the kalman gain times the innovation
+        #
+        #   x_updated = x_predicition + kalman_update = x_prediction + K * (z - H * x)
+        #
+        # Here z is the measurement, K is the Kalman gain and x is the state
+        #
+        # kalman update is x_hat = x_prediction + K * (z - H * x_prediction)
+        n = self.n + 1
+        m = self.m + (kalman_update - self.m) / n
+        return PredictionError(m, n, self.M + (kalman_update - m).T * (kalman_update - m))
+
+    def process_noise(self):
+        if self.n == 0:
+            return 1.0
+
+        return self.M / self.n
+
+
+@dataclass(slots=True)
+class FilterState:
+    state: np.ndarray
+    cov: np.ndarray
+
+    # Serves as an estimate of the process noise. We do an online estimate
+    # of this.
+    prediction_error: PredictionError = PredictionError()
+
+    def with_state(self, state, cov):
+        return FilterState(state, cov, self.prediction_error)
+
+    def update_process_error(self, state, cov, kalman_update):
+        return FilterState(state, cov, self.prediction_error.with_update(kalman_update))
+
 
 class KalmanFilter:
     def __init__(
         self,
-        initial_state,
-        initial_cov,
         transition_matrix,
         observation_matrix,
-        process_noise=None,
+        # process_noise=None,
         measurement_noise=None,
-        validate=True,
     ):
-        dim = len(initial_state)
+        self.dim = transition_matrix.shape[0]
         self.transition_matrix = transition_matrix
         self.observation_matrix = observation_matrix
-        self.process_noise = process_noise
+        # self.process_noise = process_noise
         self.measurement_noise = measurement_noise
-        self.inverse_system_uncertainty = np.full((dim, dim), np.inf)
 
-        if validate:
-            assert len(initial_state) == self.transition_matrix.shape[0]
-            assert len(initial_state) == self.transition_matrix.shape[1]
-            assert len(initial_state) == self.observation_matrix.shape[1]
-            assert self.measurement_noise.shape == (self.observation_matrix.shape[0],) * 2
-            assert self.process_noise.shape == (initial_state.shape[0],) * 2
+        # Check matrix consistency
+        assert self.transition_matrix.shape[1] == self.dim
+        assert self.observation_matrix.shape[1] == self.dim
+        assert self.measurement_noise.shape == (self.observation_matrix.shape[0],) * 2
+        # assert self.process_noise.shape == (self.dim, self.dim)
 
-        self.state = initial_state
-        self.cov = initial_cov
-
-    def timestep(self):
+    def timestep(self, state):
         """Predict a state update"""
-        self.state = self.transition_matrix @ self.state
-        self.cov = self.transition_matrix @ self.cov @ self.transition_matrix.T + self.process_noise
+        return state.with_state(
+            self.transition_matrix @ state.state,
+            self.transition_matrix @ state.cov @ self.transition_matrix.T
+            + state.prediction_error.process_noise(),
+        )
 
-    def update_state(self, measurement):
-        """Perform a Kalman update step"""
-        cov_observed = self.cov @ self.observation_matrix.T
+    def inverse_system_uncertainty(self, state):
+        cov_observed = state.cov @ self.observation_matrix.T
         system_uncertainty = self.observation_matrix @ cov_observed + self.measurement_noise
-        self.inverse_system_uncertainty = np.linalg.pinv(system_uncertainty)
+        return np.linalg.pinv(system_uncertainty)
 
-        gain = cov_observed @ self.inverse_system_uncertainty
-        self.state = self.state + gain @ (measurement - self.observation_matrix @ self.state)
-        self.cov = (np.eye(len(self.state)) - gain @ self.observation_matrix) @ self.cov
+    def add_measurement(self, state, measurement):
+        """Perform a Kalman update step"""
+        cov_observed = state.cov @ self.observation_matrix.T
+        gain = cov_observed @ self.inverse_system_uncertainty(state)
 
-    @property
-    def measurement_pdf(self):
+        # State update is the gain times innovation
+        state_update = gain @ (measurement - self.observation_matrix @ state.state)
+
+        new_state = state.update_process_error(
+            state.state + state_update,
+            (np.eye(self.dim) - gain @ self.observation_matrix) @ state.cov,
+            state_update,
+        )
+
+        return new_state
+
+    def measurement_pdf(self, state):
         """Determines the measurement pdf based on the current state"""
         h = self.observation_matrix  # shorthand
-        return h @ self.state, h @ self.cov @ h.T + self.measurement_noise
+        return h @ state.state, h @ state.cov @ h.T + self.measurement_noise
 
-    def predict_measurement(self, measurement):
+    def predict(self, state, measurement):
         """Determine measurement probability densities"""
         from scipy.stats import multivariate_normal
 
-        meas, meas_cov = self.measurement_pdf
+        meas, meas_cov = self.measurement_pdf(state)
         return multivariate_normal.pdf(measurement, mean=meas, cov=meas_cov)
 
-    def mahalanobis(self, measurement):
-        """ "Mahalanobis distance of measurement.
 
-        Returns
-        -------
-        mahalanobis : float
-        """
-        return np.sqrt(float(measurement.T @ self.inverse_system_uncertainty @ measurement))
-
-
-def generate_constant_velocity_model(
-    x0, cov, dt=1, process_noise=0.001, observation_noise=0.1, n_obs=1
-):
-    observation_matrix = np.zeros((n_obs, len(x0)))
+def generate_constant_velocity_model(n_states=2, dt=1, observation_noise=0.1, n_obs=1):
+    observation_matrix = np.zeros((n_obs, n_states))
     observation_matrix[0, 0] = 1
     return KalmanFilter(
         transition_matrix=np.array([[1.0, dt], [0.0, 1.0]]),
         observation_matrix=observation_matrix,
-        process_noise=np.eye(len(x0)) * process_noise,
         measurement_noise=np.eye(n_obs) * observation_noise,
-        initial_state=x0,
-        initial_cov=cov,
     )
 
 
-def generate_diffusion_model(x0, cov, process_noise=0.01, observation_noise=0.1, n_obs=1):
-    observation_matrix = np.zeros((n_obs, len(x0)))
+def generate_diffusion_model(n_states=2, dt=1, observation_noise=0.1, n_obs=1):
+    observation_matrix = np.zeros((n_obs, n_states))
     observation_matrix[0, 0] = 1
     return KalmanFilter(
         transition_matrix=np.array([[1.0, 0.0], [0.0, 1.0]]),
         observation_matrix=observation_matrix,
-        process_noise=np.eye(len(x0)) * process_noise,
         measurement_noise=np.eye(n_obs) * observation_noise,
-        initial_state=x0,
-        initial_cov=cov,
     )
