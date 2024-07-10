0b468e5a110a8ffbd30391df021a3981ce6bb840
Joep Vanlier <joep.vanlier@gmail.com>
2024-07-10 13:13:36 +0200

    psd: simplify constructor
    Three reasons for doing this
    - A simpler constructor will allow easier construction of tests.
    - Keeping track of the high frequency data and performing the downsampling and exclusion/fit ranges lazily will allow us to do multiple analyses without the requirement to always recompute the expensive part (the FFT).
    - Keeping track of the uncut data will allow us to plot what we excluded.

--- lumicks/pylake/force_calibration/tests/test_power_model.py
+++ lumicks/pylake/force_calibration/tests/test_power_model.py
@@ -50,14 +50,14 @@ def test_spectrum_parameter_scaling(reference_models):
 @pytest.mark.parametrize(
     "corner_frequency,diffusion_constant,num_samples,sigma_fc,sigma_diffusion",
     [
-        [1000, 1e-9, 30000, 21.113382377506746, 1.1763968470146817e-11],
-        [1500, 1.2e-9, 50000, 28.054469036154266, 1.5342555193009045e-11],
-        [1500, 1.2e-9, 10000, 28.068761746382837, 1.5365711725327977e-11],
-        [1500, 1.2e-9, 10000, 28.068761746382837, 1.5365711725327977e-11],
-        [1500, 1.2e-9, 10000, 28.068761746382837, 1.5365711725327977e-11],
-        [1500, 1.2e-9, 10000, 28.068761746382837, 1.5365711725327977e-11],
-        [1000, 1e-9, 30000, 21.113382377506746, 1.1763968470146817e-11],
-        [1000, 1e-9, 30000, 21.113382377506746, 1.1763968470146817e-11],
+        [1000, 1e-9, 30000, 21.255492602472234, 1.1778290972794654e-11],
+        [1500, 1.2e-9, 50000, 28.18419978206862, 1.536264614183137e-11],
+        [1500, 1.2e-9, 10000, 28.184199782068603, 1.5362646141831333e-11],
+        [1500, 1.2e-9, 10000, 28.184199782068603, 1.5362646141831333e-11],
+        [1500, 1.2e-9, 10000, 28.184199782068603, 1.5362646141831333e-11],
+        [1500, 1.2e-9, 10000, 28.184199782068603, 1.5362646141831333e-11],
+        [1000, 1e-9, 30000, 21.255492602472234, 1.1778290972794654e-11],
+        [1000, 1e-9, 30000, 21.255492602472234, 1.1778290972794654e-11],
     ],
 )
 def test_fit_analytic(
@@ -69,12 +69,11 @@ def test_fit_analytic(
     data = np.fft.irfft(np.sqrt(np.abs(power_spectrum) * 0.5), norm="forward")
 
     num_points_per_block = 20
-    fit_range = (0, 15000)
-    ps = PowerSpectrum(data, sample_rate=len(data))
-    ps = ps.in_range(*fit_range)
+    ps = PowerSpectrum.from_data(data, sample_rate=len(data))
+    ps = ps.in_range(1e1, 1e4)
     ps = ps.downsampled_by(num_points_per_block)
 
-    fit = fit_analytical_lorentzian(ps.in_range(1e1, 1e4))
+    fit = fit_analytical_lorentzian(ps)
 
     np.testing.assert_allclose(fit.fc, corner_frequency, rtol=1e-5)
     np.testing.assert_allclose(fit.D, diffusion_constant, rtol=1e-5, atol=0)
@@ -83,9 +82,12 @@ def test_fit_analytic(
 
 
 def test_analytic_corner_case():
-    ps = PowerSpectrum([1, 2, 3], 78125)
-    ps.frequency = np.array([178.63690424, 335.92231409, 493.20772395, 650.4931338, 807.77854365])
-    ps.power = np.array([0.00029835, 0.00027251, 0.00027432, 0.00028302, 0.00029127])
+    ps = PowerSpectrum(
+        np.array([178.63690424, 335.92231409, 493.20772395, 650.4931338, 807.77854365]),
+        np.array([0.00029835, 0.00027251, 0.00027432, 0.00028302, 0.00029127]),
+        sample_rate=78125,
+        total_duration=1.0,
+    )
     fit = fit_analytical_lorentzian(ps)
     assert fit.D > 0
 
@@ -99,13 +101,13 @@ def test_analytic_low_frequency(reference_models):
     data = power_model_to_time_series(
         78125, 78125, lambda f: reference_models.lorentzian(f, 35, 0.33)
     )
-    ps = PowerSpectrum(data, sample_rate=78125).in_range(1e2, 1e4)
+    ps = PowerSpectrum.from_data(data, sample_rate=78125).in_range(1e2, 1e4)
     fit = fit_analytical_lorentzian(ps)
     np.testing.assert_allclose(fit.fc, 0.5 * ps.frequency[0])
 
 
 def test_fit_analytic_curve():
-    ps = PowerSpectrum([3, 3, 4, 5, 1, 3, 2, 4, 5, 2], 100)
+    ps = PowerSpectrum.from_data([3, 3, 4, 5, 1, 3, 2, 4, 5, 2], 100)
     ref = [0.079276, 0.077842, 0.073833, 0.067997, 0.061221, 0.054269]
     fit = fit_analytical_lorentzian(ps)
     np.testing.assert_allclose(fit.ps_fit.frequency, np.arange(0, 60, 10))
